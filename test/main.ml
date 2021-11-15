66963be77795c101a5659052b0879193d9573fb8
zapashcanon <leo@ndrs.fr>
2021-11-16 00:42:35 +0100

    add test from spec

--- test/main.ml
+++ test/main.ml
@@ -1,1 +1,19 @@
-let () = assert true (* TODO *)
+let test_file f =
+  Format.printf "testing file: `%a`@." Fpath.pp f;
+  match Bos.OS.File.read f with
+  | Ok s ->
+    let lexbuf = Sedlexing.Utf8.from_string s in
+    Woi.Handle.process lexbuf
+  | Error (`Msg e) -> begin
+      Format.eprintf "error: %s@." e;
+      exit 1
+    end
+
+let () =
+  match Bos.OS.Dir.contents ~rel:false Fpath.(v "reference") with
+  | Ok l -> List.iter test_file l
+  | Error (`Msg e) ->
+    begin
+      Format.eprintf "error: %s@." e;
+      exit 1
+    end
