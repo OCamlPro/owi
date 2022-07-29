1f4a89dd12e3234083dde8298ec8e73ae97d22c4
Pierre Chambart <pierre.chambart@ocamlpro.com>
2022-07-29 12:47:37 +0200

    clean a bit

--- src/simplify_bis.ml
+++ src/simplify_bis.ml
@@ -542,11 +542,10 @@ module Rewrite_indices = struct
         List.map (fun ind -> { ind with value = f ind.value }) named.values
     }
 
-  let id x = x
 
   let run (module_ : assigned_module) : result =
     let global =
-      rewrite_named (rewrite_runtime (rewrite_global module_) id) module_.global
+      rewrite_named (rewrite_runtime (rewrite_global module_) Fun.id) module_.global
     in
     let elem = rewrite_named (rewrite_elem module_) module_.elem in
     let data = rewrite_named (rewrite_data module_) module_.data in
