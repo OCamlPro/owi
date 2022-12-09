daa22bb2bf2a0e3d99fb7a23ebc4b2cdfa83d7a8
zapashcanon <leo@ndrs.fr>
2022-12-09 18:18:39 +0100

    clean owi.ml

--- src/bin/owi.ml
+++ src/bin/owi.ml
@@ -1,39 +1,5 @@
 open Owi
 
-let link_state = Link.empty_state
-
-(* TODO: put this in examples
-
-   let extern_module : Link.extern_module =
-     let module M = struct
-       let rint : int32 ref Value.Extern_ref.ty = Value.Extern_ref.fresh "int ref"
-
-       let fresh i = ref i
-
-       let set r (i : int32) = r := i
-
-       let get r : int32 = !r
-     end in
-     let print_i32 (i : Int32.t) = Printf.printf "%li\n%!" i in
-     let functions =
-       [ ( "print_i32"
-         , Value.Func.Extern_func (Func (Arg (I32, Res), R0), print_i32) )
-       ; ( "fresh"
-         , Value.Func.Extern_func
-             (Func (Arg (I32, Res), R1 (Externref M.rint)), M.fresh) )
-       ; ( "set_i32r"
-         , Value.Func.Extern_func
-             (Func (Arg (Externref M.rint, Arg (I32, Res)), R0), M.set) )
-       ; ( "get_i32r"
-         , Value.Func.Extern_func
-             (Func (Arg (Externref M.rint, Res), R1 I32), M.get) )
-       ]
-     in
-     { functions }
-
-   let link_state = Link.extern_module "stuff" extern_module Link.empty_state in
-*)
-
 let simplify_then_link_then_run file =
   let to_run, _link_state =
     List.fold_left
@@ -46,7 +12,7 @@ let simplify_then_link_then_run file =
         | Types.Register (name, id) ->
           (to_run, Link.register_module state ~name ~id)
         | _ -> acc )
-      ([], link_state) file
+      ([], Link.empty_state) file
   in
   List.iter
     (fun m ->
