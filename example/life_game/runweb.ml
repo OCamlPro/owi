e1f068229defc90c2f2798c7968f9f9e228ba6d9
epatrizio <epatrizio@mpns.fr>
2023-04-18 16:05:05 +0200

    tiny_httpd migration

--- example/life_game/runweb.ml
+++ example/life_game/runweb.ml
@@ -1,30 +1,29 @@
-open Lwt
-open Cohttp_lwt_unix
+module S = Tiny_httpd
 
 let asset_loader path =
   match Content.read path with None -> assert false | Some asset -> asset
 
-let server =
-  let callback _conn req body =
-    let uri = req |> Request.uri |> Uri.to_string in
-    let l = String.split_on_char '/' uri in
-    let content, mime =
-      if List.mem "life_browser.js" l then
-        (asset_loader "life_browser.js", "application/javascript")
-      else if List.mem "life.wasm" l then
-        (asset_loader "life.wasm", "application/wasm")
-      else (asset_loader "index.html", "text/html")
-    in
-
-    (body |> Cohttp_lwt.Body.to_string >|= fun _body -> content) >>= fun body ->
-    let headers = Cohttp.Header.init () in
-    let headers = Cohttp.Header.add headers "Content-Type" mime in
-    Server.respond_string ~headers ~status:`OK ~body ()
-  in
-
-  Server.create ~mode:(`TCP (`Port 8000)) (Server.make ~callback ())
-
 let () =
+  let server = S.create ~port:8000 () in
+  S.add_route_handler ~meth:`GET server S.Route.return (fun _req ->
+    S.Response.make_string
+      ~headers:[ ("Content-Type", "text/html") ]
+      (Ok (asset_loader "index.html")) );
+
+  S.add_route_handler ~meth:`GET server
+    S.Route.(exact "life_browser.js" @/ return)
+    (fun _req ->
+      S.Response.make_string
+        ~headers:[ ("Content-Type", "application/javascript") ]
+        (Ok (asset_loader "life_browser.js")) );
+
+  S.add_route_handler ~meth:`GET server
+    S.Route.(exact "life.wasm" @/ return)
+    (fun _req ->
+      S.Response.make_string
+        ~headers:[ ("Content-Type", "application/wasm") ]
+        (Ok (asset_loader "life.wasm")) );
+
+  Printf.printf "listening on http://%s:%d\n%!" (S.addr server) (S.port server);
   ignore @@ Sys.command "xdg-open http://localhost:8000";
-  ignore (Lwt_main.run server);
-  ()
+  ignore (match S.run server with Ok () -> () | Error e -> raise e)
