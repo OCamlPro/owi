c38658f0e6ab09bd845b7210319aeb6a540ac9ef
epatrizio <epatrizio@mpns.fr>
2023-06-13 15:53:23 +0200

    Fuzzer in progress (bug fix memarg align Issue #42)

--- src/types.ml
+++ src/types.ml
@@ -533,8 +533,14 @@ struct
 
     (* TODO: when offset is 0 then do not print anything, if offset is N (memargN) then print nothing ? *)
     let memarg fmt { offset; align } =
+      let rec pow_2 n =
+        match n with
+        | 0 -> 1
+        | n when n > 0 -> 2 * pow_2 (n-1)
+        | _ -> assert false
+      in
       if offset = 0 && align = 0 then ()
-      else Format.fprintf fmt "offset=%d align=%d" offset align
+      else Format.fprintf fmt "offset=%d align=%d" offset (pow_2 align)
 
     let rec instr fmt = function
       | I32_const i -> Format.fprintf fmt "i32.const %ld" i
