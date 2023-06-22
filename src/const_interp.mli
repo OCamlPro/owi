753252784c001210b91812e83eab4fa664d9cdd8
Pierre Chambart <pierre.chambart@ocamlpro.com>
2023-06-22 11:43:16 +0200

    Split const_interp

---
+++ src/const_interp.mli
@@ -0,0 +1,2 @@
+val exec_expr :
+  Link_env.Build.t -> Simplified.Const.expr -> Link_env.t' Value.t Result.t
--- src/link.ml
+++ src/link.ml
@@ -36,60 +36,6 @@ type extern_module = { functions : (string * Value.Func.extern_func) list }
 let empty_state =
   { by_name = StringMap.empty; by_id = StringMap.empty; last = None }
 
-module Const_interp = struct
-  type env = Env.Build.t
-
-  let exec_ibinop stack nn (op : Const.ibinop) =
-    match nn with
-    | S32 ->
-      let (n1, n2), stack = Stack.pop2_i32 stack in
-      Stack.push_i32 stack
-        (let open Int32 in
-         match op with Add -> add n1 n2 | Sub -> sub n1 n2 | Mul -> mul n1 n2 )
-    | S64 ->
-      let (n1, n2), stack = Stack.pop2_i64 stack in
-      Stack.push_i64 stack
-        (let open Int64 in
-         match op with Add -> add n1 n2 | Sub -> sub n1 n2 | Mul -> mul n1 n2 )
-
-  let exec_instr (env : env) (stack : Env.t' Stack.t) (instr : Const.instr) =
-    match instr with
-    | I32_const n -> ok @@ Stack.push_i32 stack n
-    | I64_const n -> ok @@ Stack.push_i64 stack n
-    | F32_const f -> ok @@ Stack.push_f32 stack f
-    | F64_const f -> ok @@ Stack.push_f64 stack f
-    | I_binop (nn, op) -> ok @@ exec_ibinop stack nn op
-    | Ref_null t -> ok @@ Stack.push stack (Value.ref_null t)
-    | Ref_func f ->
-      let* f = Env.Build.get_func env f in
-      let value = Value.Ref (Funcref (Some f)) in
-      ok @@ Stack.push stack value
-    | Global_get id ->
-      let* g = Env.Build.get_const_global env id in
-      ok @@ Stack.push stack g
-    | Array_new _i ->
-      let len, stack = Stack.pop_i32_to_int stack in
-      (* TODO: check type of *default* *)
-      let _default, stack = Stack.pop stack in
-      let a = Array.init len (fun _i -> ()) in
-      ok @@ Stack.push_array stack a
-    | Array_new_default _i ->
-      let len, stack = Stack.pop_i32_to_int stack in
-      let a = Array.init len (fun _i -> ()) in
-      ok @@ Stack.push_array stack a
-    | I31_new ->
-      (* TODO *)
-      ok stack
-
-  let exec_expr env (e : Const.expr) : Env.t' Value.t Result.t =
-    let* stack = list_fold_left (exec_instr env) Stack.empty e in
-    match stack with
-    | [] -> Error "type mismatch (const expr returning zero values)"
-    | _ :: _ :: _ ->
-      error_s "type mismatch (const expr returning more than one value)"
-    | [ result ] -> Ok result
-end
-
 let load_from_module ls f (import : _ Imported.t) =
   match StringMap.find import.modul ls.by_name with
   | exception Not_found -> error_s "unknown module %s" import.modul
