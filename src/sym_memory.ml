500016e95a4213359defce40d6da4b7508cb1e8c
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-06-30 11:59:24 +0100

    Adds concrete load operations in sym_memory

--- src/sym_memory.ml
+++ src/sym_memory.ml
@@ -1,3 +1,6 @@
+module Intf = Interpret_functor_intf
+module Value = Sym_value.S
+
 let ( let* ) o f = Option.bind o f
 
 let ( let+ ) o f = Option.map f o
@@ -7,6 +10,8 @@ let return x = Option.Some x
 module M = struct
   module Expr = Encoding.Expression
   open Expr
+
+  let page_size = 65_536l
 
   type int32 = Expr.t
 
@@ -22,63 +27,75 @@ module M = struct
 
   let grow _m _size = assert false
 
-  let size (m : t) : int32 = Val (Num (I32 m.size))
+  let size m = Val (Num (I32 m.size))
 
-  let clone (m : t) : t =
-    { map = Hashtbl.create 0; parent = Some m; size = m.size }
+  let clone m = { map = Hashtbl.create 0; parent = Some m; size = m.size }
 
-  let size_in_pages (_m : t) : int32 = assert false
+  let size_in_pages m =
+    Val (Num (I64 (Int64.of_int32 @@ Int32.div m.size page_size)))
 
-  let concretize (addr : int32) : Int32.t =
-    match addr with Val (Num (I32 i)) -> i | _ -> assert false
+  let concretize a = match a with Val (Num (I32 i)) -> i | _ -> assert false
 
-  let rec load_byte (a : Int32.t) (m : t) : Expr.t Option.t =
+  let rec load_byte_opt a m =
     match Hashtbl.find_opt m.map a with
     | Some b -> Some b
-    | None -> Option.bind m.parent (load_byte a)
+    | None -> Option.bind m.parent (load_byte_opt a)
 
-  let load_8_s (m : t) (a : int32) : int32 =
-    let b8 = load_byte (concretize a) m in
-    Option.fold b8 ~none:(Val (Num (I32 0l))) ~some:(fun n ->
-      match n with
-      | Val (Num (I32 i)) -> Val (Num (I32 (Int32.extend_s 8 i)))
-      | e -> Extract (Val (Num (I32 0l)), 3, 0) ++ e)
+  let load_byte m a = Option.value (load_byte_opt a m) ~default:Value.I32.zero
 
-  let load_8_u (m : t) (a : Expr.t) : Expr.t =
-    let b8 = load_byte (concretize a) m in
-    Option.fold b8 ~none:(Val (Num (I32 0l))) ~some:(fun n ->
-        match n with
-        | Val (Num (I32 _)) as n' -> n'
-        | e -> Extract (Val (Num (I32 0l)), 3, 0) ++ e)
+  let concat a b offset =
+    match (a, b) with
+    | Val (Num (I32 i1)), Val (Num (I32 i2)) ->
+        let offset = Int32.of_int @@ (offset * 8) in
+        Val (Num (I32 (Int32.logor (Int32.shl i1 offset) i2)))
+    | a', b' -> Concat (a', b')
 
-  let load_16_s (mem : t) (a : Expr.t) : Expr.t =
-    let b16 =
-      let a' = concretize a in
-      let* b1 = load_byte a' mem in
-      let* b2 = load_byte (Int32.add a' 1l) mem in
-      return (Extract (Val (Num (I32 0l)), 2, 0) ++ b1 ++ b2)
+  let loadn m a n : int32 =
+    let rec loop addr n i v =
+      if i = n then v
+      else
+        let addr' = Int32.add addr @@ Int32.of_int i in
+        let byte = load_byte m addr' in
+        loop addr n (i + 1) (concat byte v i)
     in
-    Option.value b16 ~default:(Val (Num (I32 0l)))
+    let v0 = load_byte m a in
+    loop a n 1 v0
 
-  let load_16_u (mem : t) (a : Expr.t) : Expr.t =
-    (* Wrong *)
-    load_16_s mem a
+  let load_8_s m a =
+    match loadn m (concretize a) 1 with
+    | Val (Num (I32 i8)) -> Val (Num (I32 (Int32.extend_s 8 i8)))
+    | e -> Extract (Val (Num (I32 0l)), 3, 0) ++ e
 
-  let load_32 (_mem : t) (_a : Expr.t) : Expr.t = assert false
+  let load_8_u m a =
+    match loadn m (concretize a) 1 with
+    | Val (Num (I32 _)) as i8 -> i8
+    | e -> Extract (Val (Num (I32 0l)), 3, 0) ++ e
 
-  let load_64 (_mem : t) (_a : Expr.t) : Expr.t = assert false
+  let load_16_s m a =
+    match loadn m (concretize a) 2 with
+    | Val (Num (I32 i16)) -> Val (Num (I32 (Int32.extend_s 16 i16)))
+    | e -> Extract (Val (Num (I32 0l)), 2, 0) ++ e
 
-  let extract (v : int32) (h : int) (l : int) : int32 =
+  let load_16_u m a =
+    match loadn m (concretize a) 2 with
+    | Val (Num (I32 _)) as i16 -> i16
+    | e -> Extract (Val (Num (I32 0l)), 2, 0) ++ e
+
+  let load_32 m a = loadn m (concretize a) 4
+
+  let load_64 _m _a = assert false
+
+  let extract v h l =
     match v with
     | Val (Num (I32 i)) ->
-        let i' = Int32.(of_int (l * 8) |> shr_s i |> logand 0xffl) in
-        Val (Num (I32 i'))
+      let i' = Int32.(logand 0xffl @@ shr_s i @@ of_int (l * 8)) in
+      Val (Num (I32 i'))
     | Val (Num (I64 i)) ->
-        let i' = Int64.(of_int (l * 8) |> shr_s i |> logand 0xffL) in
-        Val (Num (I32 (Int32.of_int64 i')))
+      let i' = Int64.(logand 0xffL @@ shr_s i @@ of_int (l * 8)) in
+      Val (Num (I32 (Int32.of_int64 i')))
     | v' -> Extract (v', h, l)
 
-  let storen (m : t) ~(addr : int32) (v : int32) (n : int) : unit =
+  let storen m ~addr v n =
     let a0 = concretize addr in
     for i = 0 to n - 1 do
       let addr' = Int32.add a0 (Int32.of_int i)
@@ -86,15 +103,15 @@ module M = struct
       Hashtbl.replace m.map addr' v'
     done
 
-  let store_8 (m : t) ~(addr : int32) (v : int32) : unit = storen m ~addr v 1
+  let store_8 m ~addr v = storen m ~addr v 1
 
-  let store_16 (m : t) ~(addr : int32) (v : int32) : unit = storen m ~addr v 2
+  let store_16 m ~addr v = storen m ~addr v 2
 
-  let store_32 (m : t) ~(addr : int32) (v : int32) : unit = storen m ~addr v 4
+  let store_32 m ~addr v = storen m ~addr v 4
 
-  let store_64 (m : t) ~(addr : int32) (v : int64) : unit = storen m ~addr v 8
+  let store_64 m ~addr v = storen m ~addr v 8
 end
 
-module M' : Interpret_functor_intf.Memory_data = M
+module M' : Intf.Memory_data = M
 
-let memory = M.create 65536l
+let memory = M.create @@ Int32.of_int (65536 * 8)
