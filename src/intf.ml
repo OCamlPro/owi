4b13caa62907b9e41711c9c5e9fc16ed944fbd21
zapashcanon <leo@ndrs.fr>
2023-09-17 21:37:13 +0200

    remove dead code

--- src/intf.ml
+++ src/intf.ml
@@ -13,21 +13,3 @@ module type V = sig
 
   type vbool
 end
-
-module Concrete :
-  V
-    with type int32 = Int32.t
-     and type int64 = Int64.t
-     and type float32 = Float32.t
-     and type float64 = Float64.t
-     and type vbool = Bool.t = struct
-  type int32 = Int32.t
-
-  type int64 = Int64.t
-
-  type float32 = Float32.t
-
-  type float64 = Float64.t
-
-  type vbool = Bool.t
-end
