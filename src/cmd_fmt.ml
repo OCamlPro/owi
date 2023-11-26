af6592d42f39a462c9ced1d835e71b81745f5b8e
Swrup <swrup@protonmail.com>
2023-11-26 20:03:33 +0100

    read script not just module

--- src/cmd_fmt.ml
+++ src/cmd_fmt.ml
@@ -4,25 +4,23 @@
 
 open Syntax
 
-let format_file ~unsafe filename =
+let read_script filename =
   if not @@ Sys.file_exists filename then
     error_s "file `%s` doesn't exist" filename
-  else
-    let* modul = Parse.Module.from_file ~filename in
-    Compile.until_check ~unsafe modul
+  else Parse.Script.from_file ~filename
 
-let cmd debug unsafe inplace (file : string) =
+let cmd debug _unsafe inplace (file : string) =
   if debug then Log.debug_on := true;
-  match format_file ~unsafe file with
-  | Ok modul ->
+  match read_script file with
+  | Ok script ->
     if inplace then
       let chan = open_out file in
       Fun.protect
         ~finally:(fun () -> close_out chan)
         (fun () ->
           let fmt = Stdlib.Format.formatter_of_out_channel chan in
-          Format.pp fmt "%a@\n" Text.pp_modul modul )
-    else Format.pp_std "%a@\n" Text.pp_modul modul
+          Format.pp fmt "%a@\n" Text.pp_script script )
+    else Format.pp_std "%a@\n" Text.pp_script script
   | Error e ->
     Format.pp_err "%s@." e;
     exit 1
