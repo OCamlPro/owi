b270f8f5b450a09642a3129927fd493d1957ba4f
Swrup <swrup@protonmail.com>
2023-11-27 02:22:35 +0100

    fmt .wat or .wast

--- src/cmd_fmt.ml
+++ src/cmd_fmt.ml
@@ -2,22 +2,31 @@
 
 open Syntax
 
-let read_script filename =
+let get_printer filename =
   if not @@ Sys.file_exists filename then
     error_s "file `%s` doesn't exist" filename
-  else Parse.Script.from_file ~filename
+  else
+    let ext = Filename.extension filename in
+    match ext with
+    | ".wat" ->
+      Result.bind (Parse.Module.from_file ~filename) (fun v ->
+          Ok (fun fmt () -> Text.pp_modul fmt v) )
+    | ".wast" ->
+      Result.bind (Parse.Script.from_file ~filename) (fun v ->
+          Ok (fun fmt () -> Text.pp_script fmt v) )
+    | _ -> error_s "unsupported file extension"
 
 let cmd inplace (file : string) =
-  match read_script file with
-  | Ok script ->
+  match get_printer file with
+  | Error e ->
+    Format.pp_err "%s@." e;
+    exit 1
+  | Ok pp ->
     if inplace then
       let chan = open_out file in
       Fun.protect
         ~finally:(fun () -> close_out chan)
         (fun () ->
           let fmt = Stdlib.Format.formatter_of_out_channel chan in
-          Format.pp fmt "%a@\n" Text.pp_script script )
-    else Format.pp_std "%a@\n" Text.pp_script script
-  | Error e ->
-    Format.pp_err "%s@." e;
-    exit 1
+          Format.pp fmt "%a@\n" pp () )
+    else Format.pp_std "%a@\n" pp ()
