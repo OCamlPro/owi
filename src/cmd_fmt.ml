9568f863a2434936e3024ca09df1a216928a2e54
Swrup <swrup@protonmail.com>
2023-11-27 16:59:00 +0100

    use let+; do not test fmt for now

--- src/cmd_fmt.ml
+++ src/cmd_fmt.ml
@@ -9,11 +9,11 @@ let get_printer filename =
     let ext = Filename.extension filename in
     match ext with
     | ".wat" ->
-      Result.bind (Parse.Module.from_file ~filename) (fun v ->
-          Ok (fun fmt () -> Text.pp_modul fmt v) )
+      let+ v = Parse.Module.from_file ~filename in
+      fun fmt () -> Text.pp_modul fmt v
     | ".wast" ->
-      Result.bind (Parse.Script.from_file ~filename) (fun v ->
-          Ok (fun fmt () -> Text.pp_script fmt v) )
+      let+ v = Parse.Script.from_file ~filename in
+      fun fmt () -> Text.pp_script fmt v
     | _ -> error_s "unsupported file extension"
 
 let cmd inplace (file : string) =
@@ -32,6 +32,5 @@ let cmd inplace (file : string) =
     else Format.pp_std "%a@\n" pp ()
 
 let format_file_to_string (file : string) =
-  match get_printer file with
-  | Error _ as e -> e
-  | Ok pp -> Ok (Format.asprintf "%a@\n" pp ())
+  let+ pp = get_printer file in
+  Format.asprintf "%a@\n" pp ()
