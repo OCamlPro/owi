9c48af2de55c8d7330a648275ee9677dc722af15
zapashcanon <leo@ndrs.fr>
2024-01-16 01:44:53 +0100

    enable -Wall

--- src/libc/src/test-comp.c
+++ src/libc/src/test-comp.c
@@ -1,45 +1,45 @@
 #include <owi.h>
 
-_Bool __VERIFIER_nondet_bool() {
+_Bool __VERIFIER_nondet_bool(void) {
   _Bool var = owi_i32();
   owi_assume(or_(var == 0, var == 1));
   return var;
 }
 
-char __VERIFIER_nondet_char() { return (char)owi_i32(); }
+char __VERIFIER_nondet_char(void) { return (char)owi_i32(); }
 
-unsigned char __VERIFIER_nondet_uchar() { return (unsigned char)owi_i32(); }
+unsigned char __VERIFIER_nondet_uchar(void) { return (unsigned char)owi_i32(); }
 
-short __VERIFIER_nondet_short() { return (short)owi_i32(); }
+short __VERIFIER_nondet_short(void) { return (short)owi_i32(); }
 
-unsigned short __VERIFIER_nondet_ushort() {
+unsigned short __VERIFIER_nondet_ushort(void) {
   return (unsigned short)owi_i32();
 }
 
-int __VERIFIER_nondet_int() { return owi_i32(); }
+int __VERIFIER_nondet_int(void) { return owi_i32(); }
 
-unsigned int __VERIFIER_nondet_uint() { return (unsigned int)owi_i32(); }
+unsigned int __VERIFIER_nondet_uint(void) { return (unsigned int)owi_i32(); }
 
-__int128 __VERIFIER_nondet_int128() { return owi_i32(); }
+__int128 __VERIFIER_nondet_int128(void) { return owi_i32(); }
 
-unsigned __int128 __VERIFIER_nondet_uint128() {
+unsigned __int128 __VERIFIER_nondet_uint128(void) {
   return (unsigned __int128)owi_i32();
 }
 
-unsigned int __VERIFIER_nondet_charp() {
+unsigned int __VERIFIER_nondet_charp(void) {
   return (unsigned int)owi_i32();
 }
 
-long __VERIFIER_nondet_long() { return (long)owi_i32(); }
+long __VERIFIER_nondet_long(void) { return (long)owi_i32(); }
 
-unsigned long __VERIFIER_nondet_ulong() { return (unsigned long)owi_i32(); }
+unsigned long __VERIFIER_nondet_ulong(void) { return (unsigned long)owi_i32(); }
 
-long long __VERIFIER_nondet_longlong() { return owi_i64(); }
+long long __VERIFIER_nondet_longlong(void) { return owi_i64(); }
 
-unsigned long long __VERIFIER_nondet_ulonglong() {
+unsigned long long __VERIFIER_nondet_ulonglong(void) {
   return (unsigned long long)owi_i64();
 }
 
-float __VERIFIER_nondet_float() { return owi_f32(); }
+float __VERIFIER_nondet_float(void) { return owi_f32(); }
 
-double __VERIFIER_nondet_double() { return owi_f64(); }
+double __VERIFIER_nondet_double(void) { return owi_f64(); }
