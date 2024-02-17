42817cd299feb6cde3d55e98828cac0d3a7901e9
zapashcanon <leo@ndrs.fr>
2024-02-17 09:14:25 +0100

    clean stack

--- src/stack.ml
+++ src/stack.ml
@@ -61,8 +61,6 @@ module type S = sig
 
   val push : t -> value -> t
 
-  val push_const_bool : t -> Stdlib.Bool.t -> t
-
   val push_bool : t -> vbool -> t
 
   val push_i32 : t -> int32 -> t
@@ -74,8 +72,6 @@ module type S = sig
   val push_i64 : t -> int64 -> t
 
   val push_const_i64 : t -> Int64.t -> t
-
-  val push_i64_of_int : t -> int -> t
 
   val push_f32 : t -> float32 -> t
 
@@ -109,8 +105,6 @@ module Make (V : Value_intf.T) :
 
   let push s v = v :: s
 
-  let push_const_bool s b = push s (I32 (V.const_i32 (if b then 1l else 0l)))
-
   let push_bool s b = push s (I32 (V.Bool.int32 b))
 
   let push_const_i32 s i = push s (I32 (V.const_i32 i))
@@ -122,8 +116,6 @@ module Make (V : Value_intf.T) :
   let push_const_i64 s i = push s (I64 (V.const_i64 i))
 
   let push_i64 s i = push s (I64 i)
-
-  let push_i64_of_int s i = push_const_i64 s (Int64.of_int i)
 
   let push_const_f32 s f = push s (F32 (V.const_f32 f))
 
@@ -151,91 +143,71 @@ module Make (V : Value_intf.T) :
     let hd, tl = pop s in
     match hd with
     | I32 n -> (n, tl)
-    | _ | (exception Empty) -> Log.err "invalid type (expected i32)"
+    | _ -> Log.err "invalid type (expected i32)"
 
   let pop2_i32 s =
-    try
-      let n2, s = pop s in
-      let n1, tl = pop s in
-      match (n1, n2) with
-      | I32 n1, I32 n2 -> ((n1, n2), tl)
-      | _ -> Log.err "invalid type (expected i32)"
-    with Empty -> Log.err "invalid type (expected i32)"
+    let n2, s = pop s in
+    let n1, tl = pop s in
+    match (n1, n2) with
+    | I32 n1, I32 n2 -> ((n1, n2), tl)
+    | _ -> Log.err "invalid type (expected i32)"
 
   let pop_i64 s =
-    try
-      let hd, tl = pop s in
-      match hd with
-      | I64 n -> (n, tl)
-      | _ -> Log.err "invalid type (expected i64)"
-    with Empty -> Log.err "invalid type (expected i64)"
+    let hd, tl = pop s in
+    match hd with
+    | I64 n -> (n, tl)
+    | _ -> Log.err "invalid type (expected i64)"
 
   let pop2_i64 s =
-    try
-      let n2, s = pop s in
-      let n1, tl = pop s in
-      match (n1, n2) with
-      | I64 n1, I64 n2 -> ((n1, n2), tl)
-      | _ -> Log.err "invalid type (expected i64)"
-    with Empty -> Log.err "invalid type (expected i64)"
+    let n2, s = pop s in
+    let n1, tl = pop s in
+    match (n1, n2) with
+    | I64 n1, I64 n2 -> ((n1, n2), tl)
+    | _ -> Log.err "invalid type (expected i64)"
 
   let pop_f32 s =
-    try
-      let hd, tl = pop s in
-      match hd with
-      | F32 f -> (f, tl)
-      | _ -> Log.err "invalid type (expected f32)"
-    with Empty -> Log.err "invalid type (expected f32)"
+    let hd, tl = pop s in
+    match hd with
+    | F32 f -> (f, tl)
+    | _ -> Log.err "invalid type (expected f32)"
 
   let pop2_f32 s =
-    try
-      let n2, s = pop s in
-      let n1, tl = pop s in
-      match (n1, n2) with
-      | F32 n1, F32 n2 -> ((n1, n2), tl)
-      | _ -> Log.err "invalid type (expected f32)"
-    with Empty -> Log.err "invalid type (expected f32)"
+    let n2, s = pop s in
+    let n1, tl = pop s in
+    match (n1, n2) with
+    | F32 n1, F32 n2 -> ((n1, n2), tl)
+    | _ -> Log.err "invalid type (expected f32)"
 
   let pop_f64 s =
-    try
-      let hd, tl = pop s in
-      match hd with
-      | F64 f -> (f, tl)
-      | _ -> Log.err "invalid type (expected f64)"
-    with Empty -> Log.err "invalid type (expected f64)"
+    let hd, tl = pop s in
+    match hd with
+    | F64 f -> (f, tl)
+    | _ -> Log.err "invalid type (expected f64)"
 
   let pop2_f64 s =
-    try
-      let n2, s = pop s in
-      let n1, tl = pop s in
-      match (n1, n2) with
-      | F64 n1, F64 n2 -> ((n1, n2), tl)
-      | _ -> Log.err "invalid type (expected f64)"
-    with Empty -> Log.err "invalid type (expected f64)"
+    let n2, s = pop s in
+    let n1, tl = pop s in
+    match (n1, n2) with
+    | F64 n1, F64 n2 -> ((n1, n2), tl)
+    | _ -> Log.err "invalid type (expected f64)"
 
   let pop_ref s =
-    try
-      let hd, tl = pop s in
-      match hd with
-      | Ref _ -> (hd, tl)
-      | _ -> Log.err "invalid type (expected ref)"
-    with Empty -> Log.err "invalid type (expected ref)"
+    let hd, tl = pop s in
+    match hd with
+    | Ref _ -> (hd, tl)
+    | _ -> Log.err "invalid type (expected ref)"
 
   let pop_as_ref s =
-    try
-      let hd, tl = pop s in
-      match hd with
-      | Ref hd -> (hd, tl)
-      | _ -> Log.err "invalid type (expected ref)"
-    with Empty -> Log.err "invalid type (expected ref)"
+    let hd, tl = pop s in
+    match hd with
+    | Ref hd -> (hd, tl)
+    | _ -> Log.err "invalid type (expected ref)"
 
   let pop_bool s =
-    try
-      let hd, tl = pop s in
-      match hd with
-      | I32 n -> (V.I32.to_bool n, tl)
-      | _ -> Log.err "invalid type (expected i32 (bool))"
-    with Empty -> Log.err "invalid type (expected i32 (bool))"
+    let hd, tl = pop s in
+    match hd with
+    | I32 n -> (V.I32.to_bool n, tl)
+    | _ -> Log.err "invalid type (expected i32 (bool))"
 
   let pop_n s n =
     (List.filteri (fun i _hd -> i < n) s, List.filteri (fun i _hd -> i >= n) s)
