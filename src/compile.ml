9e651793a54b45e34db13e6356115acbe05b8db0
zapashcanon <leo@ndrs.fr>
2024-04-24 10:19:34 +0200

    simplify compile.ml

--- src/compile.ml
+++ src/compile.ml
@@ -2,35 +2,32 @@
 (* Copyright © 2021 Léo Andrès *)
 (* Copyright © 2021 Pierre Chambart *)
 
-let ( let* ) o f = match o with Error msg -> Error msg | Ok v -> f v
+open Syntax
 
 let until_check ~unsafe m = if unsafe then Ok m else Check.modul m
 
 let until_group ~unsafe m =
   let* m = until_check ~unsafe m in
-  let* m = Grouped.of_symbolic m in
-  Ok m
+  Grouped.of_symbolic m
 
 let until_assign ~unsafe m =
   let* m = until_group ~unsafe m in
-  let* m = Assigned.of_grouped m in
-  Ok m
+  Assigned.of_grouped m
 
 let until_simplify ~unsafe m =
   let* m = until_assign ~unsafe m in
-  let* m = Rewrite.modul m in
-  Ok m
+  Rewrite.modul m
 
 let until_typecheck ~unsafe m =
   let* m = until_simplify ~unsafe m in
   if unsafe then Ok m
   else
-    let* () = Typecheck.modul m in
-    Ok m
+    let+ () = Typecheck.modul m in
+    m
 
 let until_optimize ~unsafe ~optimize m =
-  let* m = until_typecheck ~unsafe m in
-  if optimize then Ok (Optimize.modul m) else Ok m
+  let+ m = until_typecheck ~unsafe m in
+  if optimize then (Optimize.modul m) else m
 
 let until_link ~unsafe link_state ~optimize ~name m =
   let* m = until_optimize ~unsafe ~optimize m in
@@ -38,5 +35,5 @@ let until_link ~unsafe link_state ~optimize ~name m =
 
 let until_interpret link_state ~unsafe ~optimize ~name m =
   let* m, link_state = until_link link_state ~unsafe ~optimize ~name m in
-  let* () = Interpret.Concrete.modul link_state.envs m in
-  Ok link_state
+  let+ () = Interpret.Concrete.modul link_state.envs m in
+  link_state
