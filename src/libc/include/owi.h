66e108041fe2638d1c81b9a0b604ecf010f95f7e
Zhicheng HUI <huizhicheng123@gmail.com>
2024-06-14 11:44:42 +0200

    delete wrapper functions in owi.c

--- src/libc/include/owi.h
+++ src/libc/include/owi.h
@@ -1,9 +1,9 @@
 #ifndef _OWI_H
 #define _OWI_H
 
-void *owi_malloc(void *base, unsigned int size) __attribute__((import_module("summaries"))) __attribute__((import_name("malloc")));
+void *owi_malloc(void *base, unsigned int size) __attribute__((import_module("summaries"))) __attribute__((import_name("alloc")));
 
-void owi_free(void *) __attribute__((import_module("summaries"))) __attribute__((import_name("free")));
+void owi_free(void *) __attribute__((import_module("summaries"))) __attribute__((import_name("dealloc")));
 
 char owi_i8(void) __attribute__((import_module("symbolic"))) __attribute__((import_name("i8_symbol")));
 
@@ -18,8 +18,5 @@ double owi_f64(void) __attribute__((import_module("symbolic"))) __attribute__((i
 
 void owi_assume(int c) __attribute__((import_module("symbolic"))) __attribute__((import_name("assume")));
 void owi_assert(int c) __attribute__((import_module("symbolic"))) __attribute__((import_name("assert")));
-
-int and_(int, int);
-int or_(int, int);
 
 #endif
