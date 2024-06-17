b1f2f68a5f36b14f46b7c03cc4e3a89955661ada
Zhicheng HUI <huizhicheng123@gmail.com>
2024-06-17 16:04:47 +0200

    framework for e-acsl mode

--- src/bin/owi.ml
+++ src/bin/owi.ml
@@ -69,6 +69,10 @@ let profiling =
   let doc = "profiling mode" in
   Cmdliner.Arg.(value & flag & info [ "profiling"; "p" ] ~doc)
 
+let eacsl =
+  let doc = "e-acsl mode" in
+  Cmdliner.Arg.(value & flag & info [ "e-acsl" ] ~doc)
+
 let solver =
   let doc = "SMT solver to use" in
   Cmdliner.Arg.(
@@ -147,7 +151,7 @@ let c_cmd =
       const Cmd_c.cmd $ debug $ arch $ property $ testcomp $ output $ workers
       $ opt_lvl $ includes $ files $ profiling $ unsafe $ optimize
       $ no_stop_at_failure $ no_values $ deterministic_result_order $ fail_mode
-      $ concolic $ solver )
+      $ concolic $ eacsl $ solver )
 
 let fmt_cmd =
   let open Cmdliner in
