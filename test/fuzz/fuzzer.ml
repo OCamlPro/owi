f6b6cdee17ae9942282b1f933a47f53b7a6b5bfe
Arthur Carcano <arthur.carcano@ocamlpro.com>
2024-07-01 11:58:28 +0200

    Actually fuzz SMTs

--- test/fuzz/fuzzer.ml
+++ test/fuzz/fuzzer.ml
@@ -103,6 +103,37 @@ let add_test name gen (module I1 : Interprets.INTERPRET)
 let gen (conf : Env.conf) =
   Crowbar.with_printer Owi.Text.pp_modul (Gen.modul conf)
 
+let prepare_for_sym (modul : Owi.Text.modul) =
+  let open Owi.Text in
+  let open Owi.Types in
+  let process_inst inst inst_list =
+    match inst with
+    | I32_const i -> I32_const i :: Call (Text "constant_symbol") :: inst_list
+    | _ -> inst :: inst_list
+  in
+  let process_func fnc =
+    let updated_body = List.fold_right process_inst fnc.body [] in
+    { fnc with body = updated_body }
+  in
+  let process_field = function
+    | MFunc fnc -> MFunc (process_func fnc)
+    | fld -> fld
+  in
+  let import_sym_cst_i32 =
+    MImport
+      { modul = "symbolic"
+      ; name = "symbol_i32_constant"
+      ; desc =
+          Import_func
+            ( Some "symbol_i32_constant"
+            , Bt_raw (None, ([ (None, Num_type I32) ], [ Num_type I32 ])) )
+      }
+  in
+  let updated_fields =
+    import_sym_cst_i32 :: List.map process_field modul.fields
+  in
+  { modul with fields = updated_fields }
+
 let () =
   let open Interprets in
   if Param.optimize_fuzzing then
@@ -116,4 +147,10 @@ let () =
   if Param.symbolic_fuzzing then
     add_test "symbolic_fuzzing" (gen Env.Symbolic)
       (module Owi_unoptimized)
-      (module Owi_symbolic)
+      (module Owi_symbolic);
+  if Param.full_symbolic_fuzzing then
+    add_test "full symbolic_fuzzing" (gen Env.Symbolic)
+      (module Owi_unoptimized)
+      ( module Owi_symbolic_multicore (struct
+        let symbolize = prepare_for_sym
+      end) )
