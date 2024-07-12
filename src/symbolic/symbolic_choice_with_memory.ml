b7bd9b8bde845c890789dff4781eb47e799838f2
Arthur Carcano <arthur.carcano@ocamlpro.com>
2024-07-12 18:12:43 +0200

    try 2

--- src/symbolic/symbolic_choice_with_memory.ml
+++ src/symbolic/symbolic_choice_with_memory.ml
@@ -1,1 +1,5 @@
 include Symbolic_choice.Make (Thread_with_memory)
+
+let lift_mem (mem_op : 'a Symbolic_choice_without_memory.t) : 'a t =
+  Symbolic_choice.CoreImpl.State.project_state Thread_with_memory.project
+    Thread_with_memory.restore mem_op
--- src/symbolic/thread_with_memory.ml
+++ src/symbolic/thread_with_memory.ml
@@ -1,1 +1,25 @@
 include Thread.Make (Symbolic_memory)
+
+let project (th : t) : Thread_without_memory.t * _ =
+  let projected =
+    { Thread_without_memory.symbols = th.symbols
+    ; symbol_set = th.symbol_set
+    ; pc = th.pc
+    ; memories = ()
+    ; tables = th.tables
+    ; globals = th.globals
+    ; breadcrumbs = th.breadcrumbs
+    }
+  in
+  let backup = th.memories in
+  (projected, backup)
+
+let restore backup th =
+  { symbols = th.Thread_without_memory.symbols
+  ; symbol_set = th.symbol_set
+  ; pc = th.pc
+  ; memories = backup
+  ; tables = th.tables
+  ; globals = th.globals
+  ; breadcrumbs = th.breadcrumbs
+  }
