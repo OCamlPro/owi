42c15df7ecbbc0cfff858ca71edb0de3fa726d04
Zhicheng HUI <huizhicheng123@gmail.com>
2024-07-18 15:23:48 +0200

    add custom annotations in ast and parser

---
+++ src/ast/annot.ml
@@ -0,0 +1,39 @@
+type annot =
+  { annotid : string
+  ; items : item list
+  }
+
+and item =
+  | Atom of string
+  | String of string
+  | Id of string
+  | Int of string
+  | Float of string
+  | Parens of item list
+  | Annot of annot
+
+module NameMap = Map.Make (struct
+  type t = string
+
+  let compare = compare
+end)
+
+let annot_recorder : annot list NameMap.t ref = ref NameMap.empty
+
+let find_nil key map = Option.value (NameMap.find_opt key map) ~default:[]
+
+let record_annot annot =
+  let old = find_nil annot.annotid !annot_recorder in
+  annot_recorder := NameMap.add annot.annotid (annot :: old) !annot_recorder
+
+let get_annots ?name () =
+  let annots =
+    match name with
+    | Some name -> find_nil name !annot_recorder
+    | None ->
+      List.concat_map
+        (fun (_, annots) -> annots)
+        (NameMap.to_list !annot_recorder)
+  in
+  annot_recorder := NameMap.empty;
+  annots
---
+++ src/ast/annot.mli
@@ -0,0 +1,17 @@
+type annot =
+  { annotid : string
+  ; items : item list
+  }
+
+and item =
+  | Atom of string
+  | String of string
+  | Id of string
+  | Int of string
+  | Float of string
+  | Parens of item list
+  | Annot of annot
+
+val record_annot : annot -> unit
+
+val get_annots : ?name:string -> unit -> annot list
