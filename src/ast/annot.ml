7fabad71afe92a78d5e5b9ec0b5110f5f04fc281
Zhicheng HUI <huizhicheng123@gmail.com>
2024-07-19 13:21:58 +0200

    add pretty-printer of annotations

--- src/ast/annot.ml
+++ src/ast/annot.ml
@@ -1,3 +1,5 @@
+open Format
+
 type annot =
   { annotid : string
   ; items : item list
@@ -13,6 +15,24 @@ and item =
   | Annot of annot
 
 module NameMap = Map.Make (String)
+
+(* TODO: find a better way to format annotations, possibly by
+      - recording extra format information when parsing
+      - defining rules specific to each sort of annotations *)
+
+let rec pp_annot fmt annot =
+  pp fmt "(annot %a@\n  @[<b 2>%a@]@\n)" pp_string annot.annotid
+    (pp_list ~pp_sep:pp_space pp_item)
+    annot.items
+
+and pp_item fmt = function
+  | Atom atom -> pp_string fmt atom
+  | String str -> pp_string fmt str
+  | Id id -> Types.pp_id fmt id
+  | Int i -> pp_string fmt i
+  | Float f -> pp_string fmt f
+  | Parens items -> pp_list ~pp_sep:pp_space pp_item fmt items
+  | Annot annot -> pp_annot fmt annot
 
 let annot_recorder : annot list NameMap.t ref = ref NameMap.empty
 
