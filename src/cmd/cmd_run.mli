2a9db567d489d6c1527f907d8926ea324a2b7d60
Zhicheng HUI <huizhicheng123@gmail.com>
2024-09-06 14:24:15 +0200

    add weasel example

--- src/cmd/cmd_run.mli
+++ src/cmd/cmd_run.mli
@@ -2,4 +2,4 @@
 (* Copyright © 2021-2024 OCamlPro *)
 (* Written by the Owi programmers *)
 
-val cmd : bool -> bool -> bool -> bool -> bool -> Fpath.t list -> unit Result.t
+val cmd : bool -> bool -> bool -> bool -> Fpath.t list -> unit Result.t
