27c03a016bdbca0ad1f5d51123c651e33ca7df78
Simon Ser <contact@emersion.fr>
2024-11-23 17:39:47 +0100

    add getopt() stub

--- src/libc/include/unistd.h
+++ src/libc/include/unistd.h
@@ -10,5 +10,6 @@ extern char *optarg;
 /* int open(const char* pathname,int flags, ...); */
 int close(int fd);
 ssize_t read(int fd, void *buf, size_t len) __attribute__ ((weak));;
+int getopt(int argc, char *argv[], const char *optstring);
 
 #endif
