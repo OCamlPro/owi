fd0a0c4ce51d2b916ebc41632a4a070c9e9de37b
Simon Ser <contact@emersion.fr>
2024-11-23 17:39:13 +0100

    add a bunch of additional math.h stubs

--- src/libc/src/math.c
+++ src/libc/src/math.c
@@ -3,3 +3,12 @@
 // TODO: External to OWI
 double sin(double x) { return x; }
 double cos(double x) { return x; }
+double floor(double x) { return x; }
+double round(double x) { return x; }
+float powf(float x, float y) { return x; }
+float floorf(float x) { return x; }
+double ceil(double x) { return x; }
+double sqrt(double x) { return x; }
+float roundf(float x) { return x; }
+float log2f(float x) { return x; }
+float sqrtf(float x) { return x; }
