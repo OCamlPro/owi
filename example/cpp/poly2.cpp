e8e68af441117c7ba29732af01595575b4beed59
zapashcanon <leo@ndrs.fr>
2025-01-31 00:44:41 +0100

    rewrite c++ example with a class

--- example/cpp/poly2.cpp
+++ example/cpp/poly2.cpp
@@ -1,25 +1,28 @@
 #include <owi.h>
 
+class Poly {
+  public:
+    int poly;
+    Poly(int a, int b, int c, int d) {
+      int x = owi_i32();
+      int x2 = x * x;
+      int x3 = x * x * x;
+      poly = a * x3 + b * x2 + c * x + d;
+      owi_assume(x != 1);
+      owi_assume(x != 2);
+      // Make model output deterministic
+      owi_assume(x > -2147483646);
+      owi_assume(x != 4);
+    }
+    int getPoly() {
+      return this->poly;
+    }
+};
+
 int main() {
-  int x = owi_i32();
-  int x2 = x * x;
-  int x3 = x * x * x;
+  Poly p(1, -7, 14, -8);
 
-  int a = 1;
-  int b = -7;
-  int c = 14;
-  int d = -8;
-
-  int poly = a * x3 + b * x2 + c * x + d;
-
-  owi_assume(x != 1);
-  owi_assume(x != 2);
-  owi_assume(x != 4);
-
-  // Make model output deterministic
-  owi_assume(x > -2147483646);
-
-  owi_assert(poly != 0);
+  owi_assert(p.getPoly() != 0);
 
   return 0;
 }
