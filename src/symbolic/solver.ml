ffb61521139a1fb77c6d84d9390262ff4eb5c211
zapashcanon <leo@ndrs.fr>
2025-02-21 22:33:44 +0100

    try to use a global cache

--- src/symbolic/solver.ml
+++ src/symbolic/solver.ml
@@ -10,17 +10,32 @@ let fresh solver () =
   let module Mapping = (val Smtml.Solver_dispatcher.mappings_of_solver solver)
   in
   let module Mapping = Mapping.Fresh.Make () in
-  let module Batch = Smtml.Solver.Cached (Mapping) in
+  let module Batch = Smtml.Solver.Batch (Mapping) in
   let solver = Batch.create ~logic:QF_BVFP () in
   S ((module Batch), solver)
 
-let check (S (solver_module, s)) pc =
-  let module Solver = (val solver_module) in
-  Solver.check_set s pc
+let check =
+  let module Cache = Smtml.Cache.Strong in
+  let cache = Cache.create 512 in
+  let lock = Mutex.create () in
+  fun (S (solver_module, s)) pc ->
+    Mutex.lock lock;
+    let satisfiability = Cache.find_opt cache pc in
+    Mutex.unlock lock;
+    match satisfiability with
+    | Some satisfiability -> satisfiability
+    | None ->
+      let module Solver = (val solver_module) in
+      let satisfiability = Solver.check_set s pc in
+      Mutex.lock lock;
+      Cache.add cache pc satisfiability;
+      Mutex.unlock lock;
+      satisfiability
 
 let model (S (solver_module, s)) ~symbols ~pc =
   let module Solver = (val solver_module) in
   let model =
+    (* we don't memoïze this call, because check_set *must* be used before calling model *)
     match Solver.check_set s pc with
     | `Sat -> begin
       match Solver.model ?symbols s with
