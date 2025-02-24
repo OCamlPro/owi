2e9a458edf6686e304bcfc3cf25a229af63831f7
Arthur Carcano <arthur.carcano@ocamlpro.com>
2025-02-24 19:07:25 +0100

    Improve Rust 2

--- src/rust/src/owi_sym.rs
+++ src/rust/src/owi_sym.rs
@@ -1,13 +1,16 @@
 pub mod alloc;
 mod symbolic;
-pub use symbolic::Symbolic;
+pub use symbolic::{Symbolic, SymbolicInBounds};
 
 mod sys {
     #[link(wasm_import_module = "symbolic")]
     extern "C" {
+        pub(super) fn bool_symbol() -> bool;
         pub(super) fn i8_symbol() -> u8;
         pub(super) fn i32_symbol() -> u32;
         pub(super) fn f32_symbol() -> f32;
+        pub(super) fn i64_symbol() -> u64;
+        pub(super) fn f64_symbol() -> f64;
         pub(super) fn assert(condition: bool);
         pub(super) fn assume(condition: bool);
     }
@@ -16,13 +19,6 @@ mod sys {
     extern "C" {
         pub fn alloc(base: *mut u8, size: u32) -> *mut u8;
         pub fn dealloc(base: *mut u8) -> *mut u8;
-    }
-}
-
-pub fn stop_exploration() -> ! {
-    unsafe {
-        sys::assume(false);
-        std::process::abort()
     }
 }
 
@@ -60,4 +56,16 @@ pub fn assert(b: bool) {
 
 pub fn assume(b: bool) {
     unsafe { sys::assume(b) }
+}
+
+pub fn stop_exploration() -> ! {
+    unsafe {
+        sys::assume(false);
+        std::process::abort()
+    }
+}
+
+pub fn ascii_char() -> char {
+    let code = u8::symbol_in(..=127);
+    unsafe { char::from_u32_unchecked(code as u32) }
 }
--- src/rust/src/symbolic.rs
+++ src/rust/src/symbolic.rs
@@ -1,7 +1,15 @@
-use crate::{stop_exploration, sys};
+use std::ops::RangeBounds;
 
-pub trait Symbolic {
+use crate::{assert, stop_exploration, sys};
+
+pub trait Symbolic : Sized {
     fn symbol() -> Self;
+
+    fn symbol_so_that(f: impl FnOnce(&Self) -> bool) -> Self {
+        let x = Self::symbol();
+        assert(f(&x));
+        x
+    }
 }
 
 macro_rules! impl_symbolic {
@@ -14,11 +22,15 @@ macro_rules! impl_symbolic {
     };
 }
 
+impl_symbolic!(bool, bool_symbol);
 impl_symbolic!(u8, i8_symbol);
 impl_symbolic!(i8, i8_symbol);
 impl_symbolic!(i32, i32_symbol);
 impl_symbolic!(u32, i32_symbol);
 impl_symbolic!(f32, f32_symbol);
+impl_symbolic!(i64, i64_symbol);
+impl_symbolic!(u64, i64_symbol);
+impl_symbolic!(f64, f64_symbol);
 
 impl Symbolic for char {
     fn symbol() -> Self {
@@ -27,3 +39,17 @@ impl Symbolic for char {
         c
     }
 }
+
+impl<const N : usize, T: Symbolic> Symbolic for [T; N] {
+    fn symbol() -> Self {
+        core::array::from_fn(|_| T::symbol())
+    }
+}
+
+pub trait SymbolicInBounds : Symbolic + PartialOrd {
+    fn symbol_in<R: RangeBounds<Self>>(range: R) -> Self {
+        Self::symbol_so_that(|x| range.contains(x))
+    }
+}
+
+impl<T: Symbolic + PartialOrd> SymbolicInBounds for T {}
