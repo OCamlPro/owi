3a1facad8d8c3592fb3536913d57b1c8af1ac738
S41d <said.zuhair@ocamlpro.com>
2025-02-27 17:39:34 +0100

    add simple tests

---
+++ test/c/klee_compat.c
@@ -0,0 +1,16 @@
+#include <klee/klee.h>
+
+int main(void) {
+    char c;
+    klee_make_symbolic(&c, sizeof(c), "char");
+    klee_assume(c == 'B');
+    klee_assert('b' - c == 32);
+
+    int n = klee_int("int");
+    klee_assume(n > 0);
+    klee_assert(n + 1 > 0);
+
+    int n_range = klee_range(-10, 10, "range");
+    klee_assert(n_range > 20);
+    return 0;
+}
