3b098ec95398cccf0bb1bda8c0c5ad0c986dad50
zapashcanon <leo@ndrs.fr>
2025-03-04 19:05:12 +0100

    some code cleaning

--- src/symbolic/symbolic_path_condition.ml
+++ src/symbolic/symbolic_path_condition.ml
@@ -14,36 +14,43 @@ let empty : t =
   let set = Smtml.Expr.Set.empty in
   { uf; set }
 
-let rec union_on_all_syms (c : Smtml.Expr.Set.t)
-  (uf : Smtml.Expr.Set.t Union_find.t) = function
-  | [] -> uf
-  | [ last_sym ] -> Union_find.add ~merge:Smtml.Expr.Set.union last_sym c uf
-  | sym0 :: (sym1 :: _tl as tl) ->
-    let uf = Union_find.union ~merge:Smtml.Expr.Set.union sym0 sym1 uf in
-    union_on_all_syms c uf tl
-
 (* TODO: should we make some normalization here? *)
 (* TODO: it would be better to split the conjunctions in many sub-conditions *)
 let add ({ uf; set } : t) (c : Symbolic_value.bool) : t =
-  let set = Smtml.Expr.Set.add c set in
-  let symbols = Smtml.Expr.get_symbols [ c ] in
-  let c = Smtml.Expr.Set.singleton c in
-  let uf = union_on_all_syms c uf symbols in
-  { uf; set }
+  match Smtml.Expr.get_symbols [ c ] with
+  | [] ->
+    Fmt.epr "Wrong condition: %a@\n" Symbolic_value.pp_int32 c;
+    assert false
+  | hd :: tl ->
+    let set = Smtml.Expr.Set.add c set in
+    (* We add the first symbol to the UF *)
+    let uf =
+      let c = Smtml.Expr.Set.singleton c in
+      Union_find.add ~merge:Smtml.Expr.Set.union hd c uf
+    in
+    (* We union-ize all symbols together, starting with the first one that has already been added *)
+    let uf, _last_sym =
+      List.fold_left
+        (fun (uf, last_sym) sym ->
+          (Union_find.union ~merge:Smtml.Expr.Set.union last_sym sym uf, sym) )
+        (uf, hd) tl
+    in
+    { uf; set }
 
 (* Turns all constraints into a set *)
 let to_set { uf = _; set } = set
 
 (* Return the set of constraints from [pc] that are relevant for [c]. *)
-let slice ({ uf; set } : t) (c : Symbolic_value.bool) : Smtml.Expr.Set.t =
+let slice ({ uf; set = _ } : t) (c : Symbolic_value.bool) : Smtml.Expr.Set.t =
   match Smtml.Expr.get_symbols [ c ] with
   | [] ->
+    assert false
     (* TODO: It means Smt.ml did not properly simplified a expression... *)
     (* assert false *)
     (* For now, we use this, it should be removed and assert false should be used instead later *)
-    set
+    (* Smtml.Expr.Set.add c set *)
   | sym0 :: _tl -> (
-    (* we need only the first symbol as all the other one should have been merged with it *)
+    (* we need only the first symbol as all the others should have been merged with it *)
     match Union_find.find_opt sym0 uf with
     | None ->
       (* if there is a symbol, it should have been added to the union-find structure before, otherwise it means `add` has not been called properly before *)
