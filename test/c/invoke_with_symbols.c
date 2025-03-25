6bbaab707591fd59fd8014c7b4b3095a3f6d7b15
zapashcanon <leo@ndrs.fr>
2025-03-25 18:11:05 +0100

    actually use a 64bits int for invoke test

--- test/c/invoke_with_symbols.c
+++ test/c/invoke_with_symbols.c
@@ -1,6 +1,6 @@
 #include <owi.h>
 
-void f(int n, long int m, float x, double y) {
+void f(int n, long long int m, float x, double y) {
     owi_assume(n == 1);
     owi_assume(y == -1234.);
     owi_assume(n > m);
