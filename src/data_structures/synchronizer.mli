41a818e8a51f42646054165968ffa780759060df
Arthur Carcano <arthur.carcano@ocamlpro.com>
2025-06-20 13:39:47 +0200

    Restore proper synchronizer APIs

--- src/data_structures/synchronizer.mli
+++ src/data_structures/synchronizer.mli
@@ -1,13 +1,11 @@
 type (!'get, !'write) t
 
 val init :
-     (unit -> 'get option)
-  -> ('write -> int -> Condition.t -> unit)
-  -> ('get, 'write) t
+  (unit -> 'get option) -> ('write -> Condition.t -> unit) -> ('get, 'write) t
 
 val get : ('get, 'write) t -> bool -> 'get option
 
-val write : 'write -> int -> ('get, 'write) t -> unit
+val write : 'write -> ('get, 'write) t -> unit
 
 val make_pledge : ('get, 'write) t -> unit
 
@@ -15,5 +13,4 @@ val end_pledge : ('get, 'write) t -> unit
 
 val fail : ('get, 'write) t -> unit
 
-val work_while :
-  ('get -> ('write -> int -> unit) -> unit) -> ('get, 'write) t -> unit
+val work_while : ('get -> ('write -> unit) -> unit) -> ('get, 'write) t -> unit
