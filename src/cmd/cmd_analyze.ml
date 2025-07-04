ac0db4b924efe52be05607ca960bc6e3ea8ccbd8
Julielanor <julie.elanor@free.fr>
2025-07-04 15:53:38 +0200

    implement analyze command correctly

--- src/cmd/cmd_analyze.ml
+++
@@ -1,8 +0,0 @@
-type mode =
-  | Call_graph
-  | Control_flow_graph
-
-let cmd ~analyze_mode ~call_graph_mode ~source_file ~entry_point =
-  match analyze_mode with
-  | Call_graph -> Cmd_call_graph.cmd ~call_graph_mode ~source_file ~entry_point
-  | _ -> Ok ()
