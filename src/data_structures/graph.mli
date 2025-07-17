661aeca7a3fa345ba17c4583cc4979dcc691099b
Julielanor <julie.elanor@free.fr>
2025-07-17 17:40:08 +0200

    change pp_cg

--- src/data_structures/graph.mli
+++ src/data_structures/graph.mli
@@ -1,12 +1,12 @@
 type 'a t
 
 type _ g =
-  | Cg : string option t g
+  | Cg : (string option * bool) t g
   | Cfg : Types.binary Types.instr Annotated.t list t g
 
-val init_cg : (int * 'a * int list) list -> int list -> 'a t
+val init_cg : (int * string option * int list) list -> int list -> (string option * bool) t
 
-val pp_cg : Format.formatter -> string option t -> unit
+val pp_cg : Format.formatter -> (string option * bool) t -> unit
 
 val init_cfg : (int * 'a) list -> (int * int * string option) list -> 'a t
 
@@ -21,5 +21,3 @@ val tarjan : 'a t -> Set.Make(Set.Make(Int)).t
 val build_scc_graph : 'a t -> 'a t t
 
 val pp_scc_graph : Format.formatter -> 'a t * 'a g -> unit
-
-val pp_scc_cg : Format.formatter -> 'a t * Set.Make(Set.Make(Int)).t -> unit
