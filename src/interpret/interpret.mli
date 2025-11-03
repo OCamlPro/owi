564e8b054924635a3b55186967edbc325ec5f865
hra687261 <hra687261@gmail.com>
2025-11-03 15:28:49 +0100

    Add flag to unset use-ite-for-select

--- src/interpret/interpret.mli
+++ src/interpret/interpret.mli
@@ -2,6 +2,8 @@
 (* Copyright © 2021-2024 OCamlPro *)
 (* Written by the Owi programmers *)
 
+val unset_use_ite_for_select : unit -> unit
+
 module Concrete : sig
   val modul :
        timeout:float option
