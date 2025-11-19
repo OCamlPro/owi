714e31cd64ab0ed4720b55fba1da8c92c7f97b98
redianthus <leo@ocaml.pro>
2025-11-19 19:08:01 +0100

    add missing `.mli`s

---
+++ src/cmd/cmd_zig.mli
@@ -0,0 +1,10 @@
+(* SPDX-License-Identifier: AGPL-3.0-or-later *)
+(* Copyright © 2021-2024 OCamlPro *)
+(* Written by the Owi programmers *)
+
+val cmd :
+     symbolic_parameters:Cmd_sym.parameters
+  -> includes:Fpath.t list
+  -> files:Fpath.t list
+  -> out_file:Fpath.t option
+  -> unit Result.t
