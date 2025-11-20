d42d5bb5287a5f862bad89617009744312e1fd74
redianthus <leo@ocaml.pro>
2025-11-20 18:33:56 +0100

    eradicate symbolic_memory_intf and symbolic_memory_make

--- src/symbolic/symbolic_memory_concretizing.mli
+++ src/symbolic/symbolic_memory_concretizing.mli
@@ -2,5 +2,105 @@
 (* Copyright © 2021-2024 OCamlPro *)
 (* Written by the Owi programmers *)
 
-(** @inline *)
-include Symbolic_memory_intf.S
+type t
+
+type collection
+
+val init : unit -> collection
+
+val clone : collection -> collection
+
+val get_memory : Env_id.t -> Concrete_memory.t -> collection -> int -> t
+
+(* val check_within_bounds : *)
+(*   t -> Smtml.Expr.t -> (Smtml.Expr.t * Symbolic_value.int32, Trap.t) result *)
+
+val realloc :
+     t
+  -> ptr:Smtml.Expr.t
+  -> size:Smtml.Expr.t
+  -> Smtml.Expr.t Symbolic_choice_without_memory.t
+
+val free : t -> Smtml.Expr.t -> Smtml.Expr.t Symbolic_choice_without_memory.t
+
+val load_8_s :
+  t -> Smtml.Expr.t -> Symbolic_value.int32 Symbolic_choice_without_memory.t
+
+val load_8_u :
+  t -> Smtml.Expr.t -> Symbolic_value.int32 Symbolic_choice_without_memory.t
+
+val load_16_s :
+  t -> Smtml.Expr.t -> Symbolic_value.int32 Symbolic_choice_without_memory.t
+
+val load_16_u :
+  t -> Smtml.Expr.t -> Symbolic_value.int32 Symbolic_choice_without_memory.t
+
+val load_32 :
+  t -> Smtml.Expr.t -> Symbolic_value.int32 Symbolic_choice_without_memory.t
+
+val load_64 :
+  t -> Smtml.Expr.t -> Symbolic_value.int32 Symbolic_choice_without_memory.t
+
+val store_8 :
+     t
+  -> addr:Smtml.Expr.t
+  -> Smtml.Expr.t
+  -> unit Symbolic_choice_without_memory.t
+
+val store_16 :
+     t
+  -> addr:Smtml.Expr.t
+  -> Smtml.Expr.t
+  -> unit Symbolic_choice_without_memory.t
+
+val store_32 :
+     t
+  -> addr:Smtml.Expr.t
+  -> Smtml.Expr.t
+  -> unit Symbolic_choice_without_memory.t
+
+val store_64 :
+     t
+  -> addr:Smtml.Expr.t
+  -> Smtml.Expr.t
+  -> unit Symbolic_choice_without_memory.t
+
+val grow : t -> Smtml.Expr.t -> unit
+
+val fill :
+     t
+  -> pos:Smtml.Expr.t
+  -> len:Smtml.Expr.t
+  -> char
+  -> unit Symbolic_choice_without_memory.t
+
+val blit :
+     t
+  -> src:Smtml.Expr.t
+  -> dst:Smtml.Expr.t
+  -> len:Smtml.Expr.t
+  -> unit Symbolic_choice_without_memory.t
+
+val blit_string :
+     t
+  -> string
+  -> src:Smtml.Expr.t
+  -> dst:Smtml.Expr.t
+  -> len:Smtml.Expr.t
+  -> unit
+
+val size : t -> Smtml.Expr.t
+
+val size_in_pages : t -> Smtml.Expr.t
+
+val get_limit_max : t -> Smtml.Expr.t option
+
+module ITbl : sig
+  type 'a t
+
+  type key
+
+  val iter : (key -> 'a -> unit) -> 'a t -> unit
+end
+
+val iter : (t ITbl.t -> unit) -> collection -> unit
