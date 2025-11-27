bbd15fee0697b466880202adbb0ba9af2d1b0508
redianthus <leo@ocaml.pro>
2025-11-27 12:44:35 +0100

    live on the edge

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+    url = "https://github.com/NixOS/nixpkgs/archive/master.tar.gz";
   }) {}
 }:
 
