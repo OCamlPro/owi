eb24f6596e03ac6686df8f6dbad9ac81fbdd961e
redianthus <leo@ocaml.pro>
2025-12-02 23:25:53 +0100

    move ref_intf to its own module

---
+++ src/intf/ref_intf.ml
@@ -0,0 +1,35 @@
+(* SPDX-License-Identifier: AGPL-3.0-or-later *)
+(* Copyright © 2021-2024 OCamlPro *)
+(* Written by the Owi programmers *)
+
+module type T = sig
+  type 'a get_ref =
+    | Null
+    | Ref_value of 'a
+    | Type_mismatch
+
+  module Extern : sig
+    type t
+
+    val cast : t -> 'x Type.Id.t -> 'x option
+  end
+
+  (* TODO; make this private and even opaque at some point *)
+  type t =
+    | Extern of Extern.t option
+    | Func of Kind.func option
+
+  val pp : Format.formatter -> t -> unit
+
+  val null : Text.heap_type -> t
+
+  val func : Kind.func -> t
+
+  val extern : 'x Type.Id.t -> 'x -> t
+
+  val is_null : t -> Bool.t
+
+  val get_func : t -> Kind.func get_ref
+
+  val get_extern : t -> 'x Type.Id.t -> 'x get_ref
+end
--- src/intf/value_intf.ml
+++ src/intf/value_intf.ml
@@ -2,11 +2,6 @@
 (* Copyright © 2021-2024 OCamlPro *)
 (* Written by the Owi programmers *)
 
-type 'a get_ref =
-  | Null
-  | Ref_value of 'a
-  | Type_mismatch
-
 module type T = sig
   type boolean
 
@@ -21,49 +16,6 @@ module type T = sig
   type v128
 
   module Boolean : Boolean_intf.T with type t := boolean and type i32 := i32
-
-  module Ref : sig
-    module Extern : sig
-      type t
-
-      val cast : t -> 'x Type.Id.t -> 'x option
-    end
-
-    (* TODO; make this private and even opaque at some point *)
-    type t =
-      | Extern of Extern.t option
-      | Func of Kind.func option
-
-    val pp : Format.formatter -> t -> unit
-
-    val null : Text.heap_type -> t
-
-    val func : Kind.func -> t
-
-    val extern : 'x Type.Id.t -> 'x -> t
-
-    val is_null : t -> Bool.t
-
-    val get_func : t -> Kind.func get_ref
-
-    val get_extern : t -> 'x Type.Id.t -> 'x get_ref
-  end
-
-  module F32 :
-    F32_intf.T
-      with type t := f32
-       and type boolean := boolean
-       and type i32 := i32
-       and type i64 := i64
-       and type f64 := f64
-
-  module F64 :
-    F64_intf.T
-      with type t := f64
-       and type boolean := boolean
-       and type f32 := f32
-       and type i32 := i32
-       and type i64 := i64
 
   module I32 :
     I32_intf.T
@@ -81,8 +33,26 @@ module type T = sig
        and type f32 := f32
        and type f64 := f64
 
+  module F32 :
+    F32_intf.T
+      with type t := f32
+       and type boolean := boolean
+       and type i32 := i32
+       and type i64 := i64
+       and type f64 := f64
+
+  module F64 :
+    F64_intf.T
+      with type t := f64
+       and type boolean := boolean
+       and type f32 := f32
+       and type i32 := i32
+       and type i64 := i64
+
   module V128 :
     V128_intf.T with type t := v128 and type i32 := i32 and type i64 := i64
+
+  module Ref : Ref_intf.T
 
   type t =
     | I32 of i32
