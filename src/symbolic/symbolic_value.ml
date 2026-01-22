5c0e34f44693c1c76f484e4b6d18638c290f6088
redianthus <leo@ocaml.pro>
2026-01-23 00:35:51 +0100

    more

--- src/symbolic/symbolic_value.ml
+++ src/symbolic/symbolic_value.ml
@@ -20,20 +20,20 @@ type f32 = Smtml.Typed.float32 Smtml.Typed.t
 
 type f64 = Smtml.Typed.float64 Smtml.Typed.t
 
-type v128 = V128.t
+type v128 = Smtml.Typed.bitv128 Smtml.Typed.t
 
 type t =
   | I32 of Smtml.Typed.bitv32 Smtml.Typed.t
   | I64 of Smtml.Typed.bitv64 Smtml.Typed.t
   | F32 of Smtml.Typed.float32 Smtml.Typed.t
   | F64 of Smtml.Typed.float64 Smtml.Typed.t
-  | V128 of V128.t
+  | V128 of Smtml.Typed.bitv128 Smtml.Typed.t
   | Ref of Ref.t
 
 let pp fmt = function
-  | I32 i -> I32.pp fmt i
-  | I64 i -> I64.pp fmt i
-  | F32 f -> F32.pp fmt f
-  | F64 f -> F64.pp fmt f
-  | V128 e -> V128.pp fmt e
+  | I32 i -> Smtml.Typed.Bitv32.pp fmt i
+  | I64 i -> Smtml.Typed.Bitv64.pp fmt i
+  | F32 f -> Smtml.Typed.Float32.pp fmt f
+  | F64 f -> Smtml.Typed.Float64.pp fmt f
+  | V128 e -> Smtml.Typed.Bitv128.pp fmt e
   | Ref r -> Ref.pp fmt r
