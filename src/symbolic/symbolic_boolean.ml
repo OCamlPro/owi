e5f99138fb0193f46c41397abafa94f981b8ba89
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-01-22 12:13:16 +0000

    Change boolean API

--- src/symbolic/symbolic_boolean.ml
+++ src/symbolic/symbolic_boolean.ml
@@ -1,20 +1,21 @@
 type t = bool Smtml.Typed.t
 
-let false_ = Smtml.Typed.false_
+let false_ = Smtml.Typed.Bool.false_
 
-let true_ = Smtml.Typed.true_
+let true_ = Smtml.Typed.Bool.true_
 
 let of_concrete (i : bool) : t = if i then true_ else false_ [@@inline]
 
-let not e = Smtml.Typed.not_ e [@@inline]
+let not e = Smtml.Typed.Bool.not e [@@inline]
 
-let or_ e1 e2 = Smtml.Typed.or_ e1 e2 [@@inline]
+let or_ e1 e2 = Smtml.Typed.Bool.or_ e1 e2 [@@inline]
 
-let and_ e1 e2 = Smtml.Typed.and_ e1 e2 [@@inline]
+let and_ e1 e2 = Smtml.Typed.Bool.and_ e1 e2 [@@inline]
 
-let pp fmt e = Smtml.Expr.pp fmt (Smtml.Typed.raw e)
+let pp fmt e = Smtml.Typed.Bool.pp fmt e [@@inline]
 
-let ite c ~if_true ~if_false = Smtml.Typed.ite c if_true if_false [@@inline]
+let ite c ~if_true ~if_false = Smtml.Typed.Bool.ite c if_true if_false
+[@@inline]
 
 let of_expr v = Smtml.Typed.unsafe v [@@inline]
 
