a3bc278b47da85022b65bad458e8a9454207fa6e
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-01-22 12:13:01 +0000

    Remaining typed operators for floats

--- src/symbolic/symbolic_f32.ml
+++ src/symbolic/symbolic_f32.ml
@@ -1,7 +1,5 @@
 type t = Smtml.Typed.float32 Smtml.Typed.t
 
-let ty = Smtml.Ty.Ty_fp 32
-
 let of_concrete (f : Float32.t) : t = Smtml.Typed.Float32.v (Float32.to_bits f)
 
 let zero = of_concrete Float32.zero
@@ -12,14 +10,13 @@ let neg x = Smtml.Typed.Float32.neg x
 
 let sqrt x = Smtml.Typed.Float32.sqrt x
 
-let ceil x = Smtml.Expr.unop ty Ceil (Smtml.Typed.raw x) |> Smtml.Typed.unsafe
+let ceil x = Smtml.Typed.Float32.ceil x
 
-let floor x = Smtml.Expr.unop ty Floor (Smtml.Typed.raw x) |> Smtml.Typed.unsafe
+let floor x = Smtml.Typed.Float32.floor x
 
-let trunc x = Smtml.Expr.unop ty Trunc (Smtml.Typed.raw x) |> Smtml.Typed.unsafe
+let trunc x = Smtml.Typed.Float32.trunc x
 
-let nearest x =
-  Smtml.Expr.unop ty Nearest (Smtml.Typed.raw x) |> Smtml.Typed.unsafe
+let nearest x = Smtml.Typed.Float32.nearest x
 
 let add x y = Smtml.Typed.Float32.add x y
 
@@ -33,15 +30,11 @@ let min x y = Smtml.Typed.Float32.min x y
 
 let max x y = Smtml.Typed.Float32.max x y
 
-let copy_sign x y =
-  Smtml.Expr.binop ty Copysign (Smtml.Typed.raw x) (Smtml.Typed.raw y)
-  |> Smtml.Typed.unsafe
+let copy_sign x y = Smtml.Typed.Float32.copysign x y
 
 let eq x y = Smtml.Typed.Float32.eq x y
 
-let ne x y =
-  Smtml.Expr.relop ty Ne (Smtml.Typed.raw x) (Smtml.Typed.raw y)
-  |> Smtml.Typed.unsafe
+let ne x y = Smtml.Typed.Float32.ne x y
 
 let lt x y = Smtml.Typed.Float32.lt x y
 
@@ -51,23 +44,20 @@ let le x y = Smtml.Typed.Float32.le x y
 
 let ge x y = Smtml.Typed.Float32.ge x y
 
-let convert_i32_s x = Smtml.Expr.cvtop ty ConvertSI32 x |> Smtml.Typed.unsafe
+let convert_i32_s x = Smtml.Typed.Float32.convert_i32_s x
 
-let convert_i32_u x = Smtml.Expr.cvtop ty ConvertUI32 x |> Smtml.Typed.unsafe
+let convert_i32_u x = Smtml.Typed.Float32.convert_i32_u x
 
-let convert_i64_s x = Smtml.Expr.cvtop ty ConvertSI64 x |> Smtml.Typed.unsafe
+let convert_i64_s x = Smtml.Typed.Float32.convert_i64_s x
 
-let convert_i64_u x = Smtml.Expr.cvtop ty ConvertUI64 x |> Smtml.Typed.unsafe
+let convert_i64_u x = Smtml.Typed.Float32.convert_i64_u x
 
-let demote_f64 x =
-  Smtml.Expr.cvtop ty DemoteF64 (Smtml.Typed.raw x) |> Smtml.Typed.unsafe
+let demote_f64 x = Smtml.Typed.Float32.demote_f64 x
 
-let reinterpret_i32 x =
-  Smtml.Expr.cvtop ty Reinterpret_int x |> Smtml.Typed.unsafe
+let reinterpret_i32 x = Smtml.Typed.Float32.reinterpret_i32 x
 
-let of_bits x = Smtml.Expr.cvtop ty Reinterpret_int x |> Smtml.Typed.unsafe
+let of_bits x = Smtml.Typed.Float32.reinterpret_i32 x
 
-let to_bits x =
-  Smtml.Expr.cvtop (Ty_bitv 32) Reinterpret_float (Smtml.Typed.raw x)
+let to_bits x = Smtml.Typed.Float32.to_bv x
 
-let pp fmt x = Smtml.Expr.pp fmt (Smtml.Typed.raw x)
+let pp fmt x = Smtml.Typed.Float32.pp fmt x
