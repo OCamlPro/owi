5f82bb95289a9dd19e081b8d7c4b9b9cb6801348
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2026-01-22 12:13:36 +0000

    Start doing typed bitvectors

--- src/symbolic/symbolic_f32.mli
+++ src/symbolic/symbolic_f32.mli
@@ -2,6 +2,6 @@ include
   F32_intf.T
     with type t = Smtml.Typed.float32 Smtml.Typed.t
      and type boolean := bool Smtml.Typed.t
-     and type i32 := Smtml.Expr.t
-     and type i64 := Smtml.Expr.t
+     and type i32 := Smtml.Typed.bitv32 Smtml.Typed.t
+     and type i64 := Smtml.Typed.bitv64 Smtml.Typed.t
      and type f64 := Smtml.Typed.float64 Smtml.Typed.t
