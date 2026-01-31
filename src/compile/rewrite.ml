2cb7207c19495533bdb3197647d4e2680cc99825
Hichem R. A. <hra687261@gmail.com>
2026-01-31 15:17:05 +0100

    Update src/compile/rewrite.ml
    Co-authored-by: Léo <16472988+redianthus@users.noreply.github.com>

--- src/compile/rewrite.ml
+++ src/compile/rewrite.ml
@@ -320,7 +320,7 @@ let rewrite_expr (assigned : Assigned.t) (locals : Text.param list)
 let rewrite_table_limits ({ is_i64; min; max } : Text.limits) :
   Binary.Table.Type.limits Result.t =
   if is_i64 then
-    let* min = try Ok (Int64.of_string_exn min) with _ -> Error `Table_size in
+    let* min = try Ok (Int64.of_string_exn min) with Failure _ -> Error `Table_size in
     let* max =
       try Ok (Option.map Int64.of_string_exn max) with _ -> Error `Table_size
     in
