0b42b2564c80a44af00803c2488fde1a9871bf46
redianthus <leo@ocaml.pro>
2026-02-03 17:21:42 +0100

    add a test with an assertion

---
+++ test/tinygo/assertion.t/check.go
@@ -0,0 +1,26 @@
+package main
+
+//go:wasm-module owi
+//export i32_symbol
+func i32_symbol() int32
+
+//go:wasm-module owi
+//export assert
+func owi_assert(bool)
+
+
+func mean1(x, y int32) int32 {
+	return (x & y) + ((x ^ y) >> 1)
+}
+
+func mean2(x, y int32) int32 {
+	return (x + y) / 2
+}
+
+//go:export check
+func check(x, y int32) {
+	owi_assert(mean1(x, y) == mean2(x, y))
+}
+
+// main is required by the tinygo compiler, even if it isn't used.
+func main() {}
