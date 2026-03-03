ab5c71b9dcd6e75cc0f36dee21d2d413ec7ab04b
redianthus <leo@ocaml.pro>
2026-03-03 17:10:47 +0100

    fix gadt typing

--- src/abs/abs_extern_func.ml
+++ src/abs/abs_extern_func.ml
@@ -3,11 +3,11 @@ module SVA = struct
 
   type i64 = Abs_value.Domain.integer
 
-  type f32
+  type f32 = Abs_value.Domain.integer
 
-  type f64
+  type f64 = Abs_value.Domain.integer
 
-  type v128
+  type v128 = Abs_value.Domain.integer
 end
 
 include Extern.Func.Make (SVA) (Result) (Concrete_memory)
