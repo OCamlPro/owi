a50fe0ea75466796488bc87f4e8514f823ca9fd6
redianthus <leo@ocaml.pro>
2026-03-09 12:33:46 +0100

    add ability to name all symbols in the C API

--- src/lang_c/libowi/src/owi.c
+++ src/lang_c/libowi/src/owi.c
@@ -22,51 +22,75 @@
                        : (T)(owi_uint64());                                    \
   })
 
+#define OWI_NAMED(type, name)                                                  \
+  type owi_named_##name(const char *n) {                                       \
+    owi_open_scope(n);                                                         \
+    type sym = owi_##name();                                                   \
+    owi_close_scope();                                                         \
+    return sym;                                                                \
+  }
+
+OWI_NAMED(int32_t, int32);
+OWI_NAMED(uint32_t, uint32);
+OWI_NAMED(int64_t, int64);
+OWI_NAMED(uint64_t, uint64);
+OWI_NAMED(float, float);
+OWI_NAMED(double, double);
+int owi_range_named(const char *name, int lo, int hi) {
+  owi_open_scope(name);
+  int sym = owi_range(lo, hi);
+  owi_close_scope();
+  return sym;
+};
+
 #ifdef __cplusplus
-bool
+#define OWI_BOOL bool
 #else
-_Bool
+#define OWI_BOOL _Bool
 #endif
-  owi_bool(void) {
-  return magic_size_unsigned(
-#ifdef __cplusplus
-      bool
-#else
-      _Bool
-#endif
-  );
-}
+
+OWI_BOOL owi_bool(void) { return magic_size_unsigned(OWI_BOOL); }
+OWI_NAMED(OWI_BOOL, bool)
 
 char owi_char(void) { return magic_size_signed(char); }
+OWI_NAMED(char, char)
 
 unsigned char owi_unsigned_char(void) {
   return magic_size_unsigned(unsigned char);
 }
+OWI_NAMED(unsigned char, unsigned_char)
 
 short owi_short(void) { return magic_size_signed(short); }
+OWI_NAMED(short, short)
 
 unsigned short owi_unsigned_short(void) {
-
   return magic_size_unsigned(unsigned short);
 }
+OWI_NAMED(unsigned short, unsigned_short)
 
 int owi_int(void) { return magic_size_signed(int); }
+OWI_NAMED(int, int)
 
 unsigned int owi_unsigned_int(void) {
   return magic_size_unsigned(unsigned int);
 }
+OWI_NAMED(unsigned int, unsigned_int)
 
 long owi_long(void) { return magic_size_signed(long); }
+OWI_NAMED(long, long)
 
 unsigned long owi_unsigned_long(void) {
   return magic_size_unsigned(unsigned long);
 }
+OWI_NAMED(unsigned long, unsigned_long)
 
 long long owi_long_long(void) { return magic_size_signed(long long); }
+OWI_NAMED(long long, long_long)
 
 unsigned long long owi_unsigned_long_long(void) {
   return magic_size_unsigned(unsigned long long);
 }
+OWI_NAMED(unsigned long long, unsigned_long_long)
 
 __int128 owi_int128(void) {
   int64_t high64 = owi_int64();
@@ -78,6 +102,7 @@ __int128 owi_int128(void) {
   high128 = high128 << 64;
   return high128 | low128;
 }
+OWI_NAMED(__int128, int128)
 
 unsigned __int128 owi_uint128(void) {
   int64_t high64 = owi_uint64();
@@ -89,3 +114,4 @@ unsigned __int128 owi_uint128(void) {
   high128 = high128 << 64;
   return high128 | low128;
 }
+OWI_NAMED(unsigned __int128, uint128)
