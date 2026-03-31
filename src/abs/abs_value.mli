9496ae156cb1a1aefe1ae330c1401f59d8342c16
Saïd Zuhair <said.zuhair@ocamlpro.com>
2026-03-31 14:43:13 +0200

    clean up the abstract part

--- src/abs/abs_value.mli
+++ src/abs/abs_value.mli
@@ -7,3 +7,22 @@ module Size : sig
 end
 
 module ADomain : Codex.Domains.Sig.BASE_WITH_INTEGER
+
+type t =
+  | I32 of ADomain.binary
+  | I64 of ADomain.binary
+
+val pp : ADomain.Context.t -> Format.formatter -> t -> unit
+
+val to_binary : t -> ADomain.binary
+
+val of_binary : Units.In_bits.t -> ADomain.binary -> t Result.t
+
+val equal : t -> t -> bool
+
+val binop :
+     Units.In_bits.t
+  -> (ADomain.binary -> ADomain.binary -> ADomain.binary)
+  -> t
+  -> t
+  -> t Result.t
