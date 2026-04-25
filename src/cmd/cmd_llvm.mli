1a1463dc79c844b1cc6cb77bce3ec15dfe9cd91c
LIN leo <leo.lin@etu.u-paris.fr>
2026-04-25 19:53:12 +0200

    fix(cmd llvm): add cmd_llvm files licence headers

--- src/cmd/cmd_llvm.mli
+++ src/cmd/cmd_llvm.mli
@@ -1,3 +1,7 @@
+(* SPDX-License-Identifier: AGPL-3.0-or-later *)
+(* Copyright © 2021-2024 OCamlPro *)
+(* Written by the Owi programmers *)
+
 val cmd :
      symbolic_parameters:Symbolic_parameters.t
   -> files:Fpath.t list
