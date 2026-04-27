0d935f4c01e2f9888ea0e38f1e49cb79dac021a3
Saïd Zuhair <said.zuhair@ocamlpro.com>
2026-04-27 16:23:43 +0200

    disable denot_concrete, rebase abs_driver

---
+++ src/abs/abs_stack.mli
@@ -0,0 +1,29 @@
+type 'el t
+
+val empty : 'el t
+
+val push : 'el t -> 'el -> 'el t
+
+val pop : 'el t -> 'el * 'el t
+
+val pop_2 : 'el t -> 'el * 'el * 'el t
+
+val pop_n : 'el t -> int -> 'el t * 'el t
+
+val pp : 'el Fmt.t -> Format.formatter -> 'el t -> unit
+
+val is_empty : 'el t -> bool
+
+val of_list : 'el list -> 'el t
+
+val to_list : 'el t -> 'el list
+
+val append : 'el t -> 'el t -> 'el t
+
+val take : int -> 'el t -> 'el t
+
+val drop : int -> 'el t -> 'el t
+
+val rev : 'el t -> 'el t
+
+val length : 'el t -> int
