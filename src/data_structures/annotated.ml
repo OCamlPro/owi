c779aaae3311d7f46a8fa59dd27835ffeaccc8e2
redianthus <leo@ocaml.pro>
2026-06-09 17:47:15 +0200

    add uuid to annotated

--- src/data_structures/annotated.ml
+++ src/data_structures/annotated.ml
@@ -7,44 +7,38 @@ module S = Set.Make (Int)
 type 'a t =
   { raw : 'a
   ; instr_counter : int Atomic.t
+  ; uuid : int
   ; mutable functions_called : S.t
   ; d_true : int array option ref
   ; d_false : int array option ref
   }
 
+let uuid =
+  let uuid = ref ~-1 in
+  fun () ->
+    incr uuid;
+    !uuid
+
 let raw { raw; _ } = raw
 
 let dummy raw =
   { raw
+  ; uuid = uuid ()
   ; instr_counter = Atomic.make 0
   ; functions_called = S.empty
   ; d_true = ref None
   ; d_false = ref None
   }
 
-let dummies l =
-  List.map
-    (fun raw ->
-      { raw
-      ; instr_counter = Atomic.make 0
-      ; functions_called = S.empty
-      ; d_true = ref None
-      ; d_false = ref None
-      } )
-    l
+let dummies l = List.map dummy l
 
 let dummy_deep raw =
   let raw = dummies raw in
-  { raw
-  ; instr_counter = Atomic.make 0
-  ; functions_called = S.empty
-  ; d_true = ref None
-  ; d_false = ref None
-  }
+  dummy raw
 
-let map f { raw; instr_counter; functions_called; d_true; d_false } =
+let map f { raw; instr_counter; uuid; functions_called; d_true; d_false } =
   let raw = f raw in
-  { raw; instr_counter; functions_called; d_true; d_false }
+  { raw; instr_counter; uuid; functions_called; d_true; d_false }
 
 let iter f { raw; _ } = f raw
 
