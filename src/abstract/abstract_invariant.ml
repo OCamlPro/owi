d54530166865ce4cf25c991b3e6cd008010a021c
Saïd Zuhair <said.zuhair@ocamlpro.com>
2026-06-10 13:36:07 +0200

    handle loop cases

--- src/abstract/abstract_invariant.ml
+++ src/abstract/abstract_invariant.ml
@@ -2,14 +2,19 @@
 (* Copyright © 2021-2026 OCamlPro *)
 (* Written by the Owi programmers *)
 
-type t = { cant_divide_by_zero : (int, unit) Hashtbl.t }
+type t = { cant_divide_by_zero : (int, bool) Hashtbl.t }
 
 let empty () =
   let cant_divide_by_zero = Hashtbl.create 512 in
   { cant_divide_by_zero }
 
 let cant_divide_by_zero { cant_divide_by_zero; _ } uuid =
-  Hashtbl.mem cant_divide_by_zero uuid
+  match Hashtbl.find_opt cant_divide_by_zero uuid with
+  | Some true -> true
+  | _ -> false
 
-let add_cant_divide_by_zero { cant_divide_by_zero; _ } uuid =
-  Hashtbl.add cant_divide_by_zero uuid ()
+let add_cant_divide_by_zero { cant_divide_by_zero; _ } uuid v =
+  match Hashtbl.find_opt cant_divide_by_zero uuid with
+  | Some false -> ()
+  | Some true -> Hashtbl.replace cant_divide_by_zero uuid v
+  | None -> Hashtbl.add cant_divide_by_zero uuid v
