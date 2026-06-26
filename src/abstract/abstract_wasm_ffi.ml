04c54757d9bd791a9834c6b85a7f5ffb222f52e3
Saïd Zuhair <said.zuhair@ocamlpro.com>
2026-06-26 14:43:15 +0200

    assume

--- src/abstract/abstract_wasm_ffi.ml
+++ src/abstract/abstract_wasm_ffi.ml
@@ -2,27 +2,24 @@
 (* Copyright © 2021-2026 OCamlPro *)
 (* Written by the Owi programmers *)
 
+open Abstract_extern_func
+open Abstract_extern_func.Syntax
+
 (* The constraint is used here to make sure we don't forget to define one of
    the expected FFI functions, this whole file is further constrained such that
    if one function of M is unused in the FFI module below, an error will be
    displayed *)
-module M = struct
-  let symbol_i32 ctx = Abstract_i32.unknown ctx
+let symbol_i32 ctx = Abstract_i32.unknown ctx
 
-  let symbol_i64 ctx = Abstract_i64.unknown ctx
+let symbol_i64 ctx = Abstract_i64.unknown ctx
 
-  let assume ctx i = Abstract_domain.assume ctx (Abstract_i32.to_boolean ctx i)
-end
-
-open M
-open Abstract_extern_func
-open Abstract_extern_func.Syntax
+let assume ctx = raise (ExternFuncException (Assume ctx))
 
 let symbolic_extern_module =
   let functions =
     [ ("i32_symbol", Extern_func (context ^->. i32, symbol_i32))
     ; ("i64_symbol", Extern_func (context ^->. i64, symbol_i64))
-      (* ; ("assume", Extern_func (context ^-> i32 ^->. unit, assume)) *)
+    ; ("assume", Extern_func (context ^-> i32 ^->. unit, assume))
       (* ; ("assert", Extern_func (i32 ^->. unit, assert')) *)
       (* ; ("exit", Extern_func (i32 ^->. unit, exit)) *)
     ]
