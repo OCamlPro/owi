02f424caa7ad0dd3a0777be8d13cb6e32f34b522
redianthus <leo@ocaml.pro>
2026-07-03 18:49:28 +0200

    it's not that bad but not perfect either

--- src/abstract/abstract_state.ml
+++ src/abstract/abstract_state.ml
@@ -2,16 +2,12 @@
 (* Copyright © 2021-2026 OCamlPro *)
 (* Written by the Owi programmers *)
 
-type extern_func
-
 type t =
   { ctx : Abstract_domain.Context.t
   ; stack : Abstract_stack.t
   ; locals : Abstract_value.t Abstract_locals.t
   ; globals : Abstract_value.t Abstract_globals.t
   ; func_rt : Binary.val_type list
-  ; env : extern_func Link_env.t
-  ; envs : extern_func Link_env.t Dynarray.t
   ; invariant : Abstract_invariant.t
   }
 
@@ -22,7 +18,7 @@ let pp ctx : t Fmt.t =
     (Fmt.list ~sep:Fmt.semi (Abstract_value.pp_with_ctx state.ctx))
     (Abstract_locals.to_list state.locals |> List.map snd)
 
-let empty env envs () =
+let empty env () =
   let ctx = Abstract_domain.root_context () in
   let stack = Abstract_stack.empty in
   let locals = Abstract_locals.empty in
@@ -40,4 +36,4 @@ let empty env envs () =
   in
   let func_rt = [] in
   let invariant = Abstract_invariant.empty () in
-  { ctx; stack; locals; env; func_rt; envs; invariant; globals }
+  { ctx; stack; locals; func_rt; invariant; globals }
