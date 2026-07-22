46621bfd5d32d69379dd71bd2f85df6713d47e60
redianthus <leo@ocaml.pro>
2026-07-22 20:50:57 +0200

    almost there

--- src/script/script_symbolic.ml
+++ src/script/script_symbolic.ml
@@ -2,154 +2,107 @@
 (* Copyright © 2021-2026 OCamlPro *)
 (* Written by the Owi programmers *)
 
-open Syntax
-
-(*
+open Symbolic_choice
 module Stack = Stack.Make [@inlined hint] (Symbolic_value)
+
 type host_externref = int
 
 let ty : host_externref Type.Id.t = Type.Id.make ()
-*)
 
-(*
-let action =
-  (link_state : Symbolic_extern_func.extern_func Link.State.t) =
-  function
+let action (link_state : Symbolic_extern_func.extern_func Link.State.t) :
+  _ -> _ Symbolic_choice.t = function
   | Wast.Invoke (mod_id, f, args) -> begin
     Log.info (fun m ->
       m "invoke %a %s %a..."
         (Fmt.option ~none:Fmt.nop Fmt.string)
         mod_id f Wast.pp_consts args );
-    let* f, env_id = Link.State.get_func_from_module link_state mod_id f in
-    let stack = List.rev_map (Symbolic_value.of_script_const ~ty) args in
-    let envs = Link.State.get_envs link_state in
-    let module I = Interpret.Symbolic (Interpret.Default_parameters) in
-    I.exec_vfunc_from_outside ~locals:stack ~env:env_id ~envs f
+    let open Syntax in
+    match Link.State.get_func_from_module link_state mod_id f with
+    | Error e -> trap e
+    | Ok (f, env_id) -> begin
+      let stack = List.rev_map (Symbolic_value.of_script_const ~ty) args in
+      let envs = Link.State.get_envs link_state in
+      let module I = Interpret.Symbolic (Interpret.Default_parameters) in
+      I.exec_vfunc_from_outside ~locals:stack ~env:env_id ~envs f
+      end
     end
-  | Get (mod_id, name) ->
+  | Get (mod_id, name) -> (
     Log.info (fun m -> m "get...");
-    let+ global = Link.State.get_global_from_module link_state mod_id name in
-    Symex.Monad.return [ global.value ]
-    *)
+    match Link.State.get_global_from_module link_state mod_id name with
+    | Error e -> trap e
+    | Ok global ->
+      let v = (* TODO: convert from concrete to symbolic *) global.value in
+      return [ v ] )
 
-(*
 let unsafe = false
-*)
 
-let run = fun ~no_exhaustion:_ _ -> assert false
-(*
-  ~no_exhaustion script =
+let run ~no_exhaustion script =
   let state =
     Link.State.empty ()
     |> Link.Extern.modul ~name:"spectest_extern" Spectest.extern_m
   in
   let script = Spectest.m :: Register ("spectest", Some "spectest") :: script in
-  let registered = ref false in
-  let curr_module = ref 0 in
   let module I = Interpret.Symbolic (Interpret.Default_parameters) in
   list_fold_left
     (fun (link_state : Symbolic_extern_func.extern_func Link.State.t) ->
       function
       | Wast.Text_module (false, m) ->
-        if !curr_module = 0 then
-          (* TODO: disable printing*)
-          ();
         Log.info (fun m -> m "*** module");
-        incr curr_module;
-        let* m, link_state =
-          Compile.Text.until_link link_state ~unsafe ~name:None m
-        in
-        let+ () = I.modul link_state m in
-        (* TODO: enable printing again! *)
-        link_state
+        begin match Compile.Text.until_link link_state ~unsafe ~name:None m with
+        | Error e -> trap e
+        | Ok (m, link_state) ->
+          let+ () = I.modul link_state m in
+          (* TODO: enable printing again! *)
+          link_state
+        end
       | Wast.Quoted_module (false, m) ->
         Log.info (fun m -> m "*** quoted module");
-        incr curr_module;
-        let* m = Parse.Text.Inline_module.from_string m in
-        let* m, link_state =
-          Compile.Text.until_link link_state ~unsafe ~name:None m
-        in
-        let+ () = I.modul link_state m in
-        link_state
+        begin match Parse.Text.Inline_module.from_string m with
+        | Error e -> trap e
+        | Ok m ->
+          begin match
+            Compile.Text.until_link link_state ~unsafe ~name:None m
+          with
+          | Error e -> trap e
+          | Ok (m, link_state) ->
+            let+ () = I.modul link_state m in
+            link_state
+          end
+        end
       | Wast.Binary_module (false, id, m) ->
         Log.info (fun m -> m "*** binary module");
-        incr curr_module;
-        let* m = Parse.Binary.Module.from_string m in
-        let m = { m with id } in
-        let* m, link_state =
-          Compile.Binary.until_link link_state ~unsafe ~name:None m
-        in
-        let+ () = I.modul link_state m in
-        link_state
+        begin match Parse.Binary.Module.from_string m with
+        | Error e -> trap e
+        | Ok m ->
+          let m = { m with id } in
+          begin match
+            Compile.Binary.until_link link_state ~unsafe ~name:None m
+          with
+          | Error e -> trap e
+          | Ok (m, link_state) ->
+            let+ () = I.modul link_state m in
+            link_state
+          end
+        end
       | Assert (Assert_trap_module (m, expected)) ->
         Log.info (fun m -> m "*** assert_trap");
-        incr curr_module;
-        let* m, link_state =
-          Compile.Text.until_link link_state ~unsafe ~name:None m
-        in
-        let got = I.modul link_state m in
-        let+ () = Script_error.check_result ~expected ~got in
-        link_state
-      | Assert (Assert_malformed_binary (m, expected)) ->
-        Log.info (fun m -> m "*** assert_malformed_binary");
-        let got = Parse.Binary.Module.from_string m in
-        let+ () = Script_error.check_result ~expected ~got in
-        link_state
-      | Assert (Assert_malformed_quote (m, expected)) ->
-        Log.info (fun m -> m "*** assert_malformed_quote");
-        (* TODO: use Parse.Text.Module.from_string instead *)
-        let got = Parse.Text.Script.from_string m in
-        let+ () =
-          match got with
-          | Error got -> Script_error.check_error ~expected ~got
-          | Ok [ Text_module (false, m) ] ->
-            let got = Compile.Text.until_binary ~unsafe m in
-            Script_error.check_result ~expected ~got
-          | _ -> assert false
-        in
-        link_state
-      | Assert (Assert_invalid_binary (m, expected)) ->
-        Log.info (fun m -> m "*** assert_invalid_binary");
-        let got = Parse.Binary.Module.from_string m in
-        let+ () =
-          match got with
-          | Error got -> Script_error.check_error ~expected ~got
-          | Ok m ->
-            begin match Binary_validate.modul m with
-            | Error got -> Script_error.check_error ~expected ~got
-            | Ok () ->
-              let got = Link.Binary.modul link_state ~name:None m in
-              Script_error.check_result ~expected ~got
-            end
-        in
-        link_state
-      | Assert (Assert_invalid (m, expected)) ->
-        Log.info (fun m -> m "*** assert_invalid");
-        let got = Compile.Text.until_link link_state ~unsafe ~name:None m in
-        let+ () = Script_error.check_result ~expected ~got in
-        link_state
-      | Assert (Assert_invalid_quote (m, expected)) ->
-        Log.info (fun m -> m "*** assert_invalid_quote");
-        let got = Parse.Text.Script.from_string m in
-        let+ () =
-          match got with
-          | Error got -> Script_error.check_error ~expected ~got
-          | Ok [ Text_module (false, m) ] ->
-            let got = Compile.Text.until_validate ~unsafe m in
-            Script_error.check_result ~expected ~got
-          | _ -> assert false
-        in
-        link_state
-      | Assert (Assert_unlinkable (m, expected)) ->
-        Log.info (fun m -> m "*** assert_unlinkable");
-        let got = Compile.Text.until_link link_state ~unsafe ~name:None m in
-        let+ () = Script_error.check_result ~expected ~got in
-        link_state
-      | Assert (Assert_malformed (m, expected)) ->
-        Log.info (fun m -> m "*** assert_malformed");
-        let got = Compile.Text.until_link ~unsafe ~name:None link_state m in
-        let+ () = Script_error.check_result ~expected ~got in
-        assert false
+        begin match Compile.Text.until_link link_state ~unsafe ~name:None m with
+        | Error e -> trap e
+        | Ok (m, link_state) ->
+          let* got = I.modul link_state m in
+          begin match Script_error.check_result ~expected ~got with
+          | Error e -> trap e
+          | Ok () -> return link_state
+          end
+        end
+      | Assert (Assert_malformed_binary _)
+      | Assert (Assert_malformed_quote _)
+      | Assert (Assert_invalid_binary _)
+      | Assert (Assert_invalid _)
+      | Assert (Assert_invalid_quote _)
+      | Assert (Assert_unlinkable _)
+      | Assert (Assert_malformed _) ->
+        return link_state
       | Assert (Assert_return (a, res)) ->
         Log.info (fun m -> m "*** assert_return");
         let* stack = action link_state a in
@@ -163,29 +116,35 @@ let run = fun ~no_exhaustion:_ _ -> assert false
         then begin
           Log.err (fun m ->
             m "got:      %a@.expected: %a" Stack.pp stack Wast.pp_results res );
-          Error `Bad_result
+          trap `Bad_result
         end
-        else Ok link_state
+        else return link_state
       | Assert (Assert_trap (a, expected)) ->
         Log.info (fun m -> m "*** assert_trap");
         let got = action link_state a in
-        let+ () = Script_error.check_result ~expected ~got in
-        link_state
+        begin match Script_error.check_result ~expected ~got with
+        | Error e -> trap e
+        | Ok () -> return link_state
+        end
       | Assert (Assert_exhaustion (a, expected)) ->
         Log.info (fun m -> m "*** assert_exhaustion");
         let+ () =
-          if no_exhaustion then Ok ()
+          if no_exhaustion then return ()
           else
             let got = action link_state a in
-            Script_error.check_result ~expected ~got
+            match Script_error.check_result ~expected ~got with
+            | Error e -> trap e
+            | Ok () -> return ()
         in
         link_state
       | Register (name, mod_name) ->
-        if !curr_module = 1 && not !registered then (* TODO: disable debug *) ();
         Log.info (fun m -> m "*** register");
-        let+ state = Link.register_last_module link_state ~name ~id:mod_name in
-        (* TODO: enable debug again! *)
-        state
+        begin match Link.register_last_module link_state ~name ~id:mod_name with
+        | Error e -> trap e
+        | Ok state ->
+          (* TODO: enable debug again! *)
+          return state
+        end
       | Action a ->
         Log.info (fun m -> m "*** action");
         let+ _stack = action link_state a in
@@ -194,14 +153,13 @@ let run = fun ~no_exhaustion:_ _ -> assert false
       | Binary_module (true, _, _)
       | Quoted_module (true, _) ->
         (* TODO: differentiate between modules and module definitions in the
-            link state, ensure that we can instantiate a module from its module
-            definition, and that module definitions are not treated as "normal",
-            or instantiated module. *)
-        Ok link_state
+              link state, ensure that we can instantiate a module from its module
+              definition, and that module definitions are not treated as "normal",
+              or instantiated module. *)
+        return link_state
       | Instance (_name, _mod_name) ->
-        Error (`Unimplemented "(module instance _)") )
+        trap (`Unimplemented "(module instance _)") )
     state script
-    *)
 
 let exec ~no_exhaustion script =
   let+ _link_state = run ~no_exhaustion script in
