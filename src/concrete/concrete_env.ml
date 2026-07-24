bc8fed3a7c957eeb31246e32996e24847e7c7c4f
redianthus <leo@ocaml.pro>
2026-07-24 16:20:35 +0200

    more cleaning

--- src/concrete/concrete_env.ml
+++ src/concrete/concrete_env.ml
@@ -2,8 +2,6 @@
 (* Copyright © 2021-2026 OCamlPro *)
 (* Written by the Owi programmers *)
 
-type t = Concrete_extern.Func.t Link.Linked_module.t
-
 let get_memory ~modul link_state id = Link.State.get_memory link_state ~modul id
 
 let get_data ~modul link_state id = Link.State.get_data link_state ~modul id
