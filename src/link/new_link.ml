75aef695cc544bf9bc049c47ca8a3ef584b424ef
redianthus <leo@ocaml.pro>
2026-07-28 00:22:38 +0200

    memory and const expr

--- src/link/new_link.ml
+++ src/link/new_link.ml
@@ -4,7 +4,60 @@
 
 [@@@warning "-27"]
 
+[@@@warning "-69"]
+
 open Syntax
+
+module Eval_const = struct
+  module Value = Concrete_value
+  module Stack = Stack.Make [@inlined hint] (Value)
+
+  let i32_instr stack : Binary.i32_instr -> _ = function
+    | Const i -> Stack.push_i32 stack i
+    | Add -> Stack.apply_i32_i32_i32 stack Value.I32.add
+    | Sub -> Stack.apply_i32_i32_i32 stack Value.I32.sub
+    | Mul -> Stack.apply_i32_i32_i32 stack Value.I32.mul
+    | _ -> assert false
+
+  let i64_instr stack : Binary.i64_instr -> _ = function
+    | Const i -> Stack.push_i64 stack i
+    | Add -> Stack.apply_i64_i64_i64 stack Value.I64.add
+    | Sub -> Stack.apply_i64_i64_i64 stack Value.I64.sub
+    | Mul -> Stack.apply_i64_i64_i64 stack Value.I64.mul
+    | _ -> assert false
+
+  let simple_instruction ~get_func ~get_global stack = function
+    | Binary.I32 i -> Result.ok (i32_instr stack i)
+    | Binary.I64 i -> Result.ok (i64_instr stack i)
+    | F32 (Const f) -> Result.ok @@ Stack.push_f32 stack f
+    | F64 (Const f) -> Result.ok @@ Stack.push_f64 stack f
+    | V128 (Const f) -> Result.ok @@ Stack.push_v128 stack f
+    | Ref (Null t) -> Result.ok @@ Stack.push_ref stack (Value.Ref.null t)
+    | Ref (Func f) ->
+      let* f = get_func f in
+      let value = Value.Ref (Func (Some f)) in
+      Result.ok @@ Stack.push stack value
+    | Global (Get id) ->
+      let* g = get_global id in
+      Result.ok @@ Stack.push stack g
+    | _ -> assert false
+
+  let instr ~get_func ~get_global stack instr =
+    match instr.Annotated.raw with
+    | Binary.Simple i -> simple_instruction ~get_func ~get_global stack i
+    | _ -> assert false
+
+  (* TODO: binary+const expr *)
+  let expr ~get_func ~get_global e : Concrete_value.t Result.t =
+    let* stack =
+      list_fold_left (instr ~get_func ~get_global) Stack.empty e.Annotated.raw
+    in
+    match stack with
+    | [] -> Error (`Type_mismatch "const expr returning zero values")
+    | _ :: _ :: _ ->
+      Error (`Type_mismatch "const expr returning more than one value")
+    | [ result ] -> Ok result
+end
 
 module Allocator : sig
   type !+'a t
@@ -28,14 +81,12 @@ end
 module IntMap = Map.Make (Int)
 module StringMap = Map.Make (String)
 
-module type Runtime_builder_intf = sig
-  type value
-end
-
 module type Runtime_intf = sig
   type t
 
   type modul
+
+  type memory
 
   type value
 
@@ -50,9 +101,23 @@ module type Runtime_intf = sig
   val link_binary_module : runtime:t -> modul:Binary.Module.t -> t Result.t
 end
 
+module type Runtime_builder_intf = sig
+  type value
+
+  type memory
+
+  val init_memory : Binary.Mem.Type.limits -> memory
+
+  val get_memory_limits : memory -> Binary.Mem.Type.limits
+
+  val value_of_concrete : Concrete_value.t -> value
+end
+
 module Make (M : Runtime_builder_intf) :
-  Runtime_intf with type value = M.value = struct
+  Runtime_intf with type value = M.value and type memory = M.memory = struct
   type value = M.value
+
+  type memory = M.memory
 
   type modul = int
 
@@ -69,8 +134,13 @@ module Make (M : Runtime_builder_intf) :
 
   module AddressMap = Map.Make (SourceId)
 
+  (* when evaluating constant expressions, we don't want to deal with value because building them is annoying and differs too much between the various interpreters, yet, the constant expression builders can read globals that could be values, but we use the fact that it can only read constant globals that are always going to be concrete, doing so allows us to have a single concrete implementation of constant evaluation, with the price of having to convert from concrete to {abstract,symbolic} each time we load a constant global, but who cares, we could simply inline them in the future and don't bother *)
+  type global_value =
+    | Const of Concrete_value.t
+    | Var of value
+
   type global =
-    { value : value
+    { value : global_value
     ; typ : Binary.Global.Type.t
     }
 
@@ -83,6 +153,10 @@ module Make (M : Runtime_builder_intf) :
         (* map from runtime address to runtime globals *)
     ; globals_map : Allocator.key AddressMap.t
         (* map from global source id to runtime address *)
+    ; memories : memory Allocator.t
+        (* map from runtime address to runtime memories *)
+    ; memories_map : Allocator.key AddressMap.t
+        (* map from memory source id to runtime address *)
     ; initialization_codes : Binary.expr IntMap.t
         (* map from modul to their initialization code *)
     ; exports : Binary.Module.Exports.t IntMap.t
@@ -97,6 +171,8 @@ module Make (M : Runtime_builder_intf) :
     let functions_map = AddressMap.empty in
     let globals = Allocator.empty in
     let globals_map = AddressMap.empty in
+    let memories = Allocator.empty in
+    let memories_map = AddressMap.empty in
     let initialization_codes = IntMap.empty in
     let exports = IntMap.empty in
     let last_module = None in
@@ -105,6 +181,8 @@ module Make (M : Runtime_builder_intf) :
     ; functions_map
     ; globals
     ; globals_map
+    ; memories
+    ; memories_map
     ; initialization_codes
     ; exports
     ; last_module
@@ -134,9 +212,6 @@ module Make (M : Runtime_builder_intf) :
     match IntMap.find_opt modul runtime.initialization_codes with
     | Some expr -> expr
     | None -> []
-
-  let eval_constant_expression (_e : Binary.expr Annotated.t) : value =
-    assert false
 
   let load_import ~(runtime : t)
     ~import:({ modul_name; name; _ } : _ Origin.imported) address_map allocator
@@ -217,7 +292,17 @@ module Make (M : Runtime_builder_intf) :
 
   let link_global ~modul id runtime = function
     | Origin.Local ({ init; typ; id = _ } : Binary.Global.t) ->
-      let value = eval_constant_expression init in
+      let* value =
+        Eval_const.expr
+          ~get_func:(fun _ -> assert false)
+          ~get_global:(fun _ -> assert false)
+          init
+      in
+      let value =
+        match fst typ with
+        | Const -> Const value
+        | Var -> Var (M.value_of_concrete value)
+      in
       let global : global = { value; typ } in
       let globals, address = Allocator.add global runtime.globals in
       let globals_map =
@@ -246,6 +331,54 @@ module Make (M : Runtime_builder_intf) :
       in
       Ok { runtime with globals_map }
 
+  let memory_limit_is_included ~import ?imported_data_size ~imported () =
+    match (import, imported) with
+    | Binary.Mem.Type.I32 import, Binary.Mem.Type.I32 imported ->
+      Int32.(
+        le_u import.min
+          (Option.fold ~none:imported.min
+             ~some:(fun v -> of_int v)
+             imported_data_size ) )
+      && begin match (imported.max, import.max) with
+      | _, None -> true
+      | None, Some _ -> false
+      | Some i, Some j -> Int32.le_u i j
+      end
+    | I64 import, I64 imported ->
+      import.min
+      <= Option.fold ~none:imported.min ~some:(fun v -> v) imported_data_size
+      && begin match (imported.max, import.max) with
+      | _, None -> true
+      | None, Some _ -> false
+      | Some i, Some j -> i <= j
+      end
+    | _ -> false
+
+  let link_memory ~modul id runtime = function
+    | Origin.Local (_label, typ) ->
+      let memory = M.init_memory typ in
+      let memories, address = Allocator.add memory runtime.memories in
+      let memories_map =
+        AddressMap.add { id; modul } address runtime.memories_map
+      in
+      Ok { runtime with memories_map; memories }
+    | Imported ({ name; typ; _ } as import) ->
+      let* memory, address =
+        load_import ~runtime ~import runtime.memories_map runtime.memories
+      in
+      (* comparing their types *)
+      let* () =
+        let imported_limit = M.get_memory_limits memory in
+        if memory_limit_is_included ~import:typ ~imported:imported_limit () then
+          Ok ()
+        else Error (`Incompatible_import_type name)
+      in
+      (* adding new memory to the address map *)
+      let memories_map =
+        AddressMap.add { id; modul } address runtime.memories_map
+      in
+      Ok { runtime with memories_map }
+
   let link_binary_module ~(runtime : t) ~(modul : Binary.Module.t) : t Result.t
       =
     let new_module = get_next_module ~runtime in
@@ -254,16 +387,25 @@ module Make (M : Runtime_builder_intf) :
       array_fold_lefti (link_function ~modul:new_module) runtime modul.func
     in
     (* tags *)
+    (* TODO *)
     (* globals *)
     let* runtime =
       array_fold_lefti (link_global ~modul:new_module) runtime modul.global
     in
     (* memories *)
+    let* runtime =
+      array_fold_lefti (link_memory ~modul:new_module) runtime modul.mem
+    in
     (* tables *)
+    (* TODO *)
     (* initialization code *)
+    (* TODO *)
     (* 1. data *)
+    (* TODO *)
     (* 2. elem *)
+    (* TODO *)
     (* 3. start function *)
+    (* TODO *)
     let initialization_code =
       match modul.Binary.Module.start with
       | None -> []
@@ -278,28 +420,44 @@ module Make (M : Runtime_builder_intf) :
     let initialization_codes =
       IntMap.add new_module initialization_code runtime.initialization_codes
     in
-    let registered_modules = runtime.registered_modules in
-    let runtime =
-      { runtime with
-        initialization_codes
-      ; exports
-      ; last_module
-      ; registered_modules
-      }
-    in
+    let runtime = { runtime with initialization_codes; exports; last_module } in
     Ok runtime
 end
 
 module Concrete_runtime_builder : Runtime_builder_intf = struct
   type value = Concrete_value.t
+
+  let value_of_concrete v = v
+
+  type memory = Concrete_memory.t
+
+  let init_memory = Concrete_memory.init
+
+  let get_memory_limits = Concrete_memory.get_limits
 end
 
 module Symbolic_runtime_builder : Runtime_builder_intf = struct
   type value = Symbolic_value.t
+
+  let value_of_concrete v = Symbolic_value.of_concrete v
+
+  type memory = Symbolic_memory.t
+
+  let init_memory _ = assert false
+
+  let get_memory_limits _ = assert false
 end
 
 module Abstract_runtime_builder : Runtime_builder_intf = struct
   type value = Abstract_value.t
+
+  let value_of_concrete _ = assert false
+
+  type memory = Abstract_memory.t
+
+  let init_memory _ = assert false
+
+  let get_memory_limits _ = assert false
 end
 
 module Interpret (Runtime : Runtime_intf) = struct
