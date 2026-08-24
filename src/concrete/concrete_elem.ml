334e8b9615e31cdb7eaf0040f16dd79ca7b0f2c2
redianthus <leo@ocaml.pro>
2026-08-24 14:36:41 +0200

    simplify elem implementation in concrete and symbolic

--- src/concrete/concrete_elem.ml
+++ src/concrete/concrete_elem.ml
@@ -2,16 +2,12 @@
 (* Copyright © 2021-2026 OCamlPro *)
 (* Written by the Owi programmers *)
 
-type t = { mutable value : Concrete_value.t Concrete_ref.t Iarray.t }
+type t = Concrete_value.t Concrete_ref.t Iarray.t
 
-let get (e : t) i = Iarray.get e.value i
+let get (e : t) i = Iarray.get e i
 
-let size (e : t) = Iarray.length e.value
+let size (e : t) = Iarray.length e
 
-let drop (e : t) =
-  e.value <- Iarray.of_list [];
-  e
+let drop (_e : t) = Iarray.of_list []
 
-let init l =
-  let value = Iarray.of_list l in
-  { value }
+let init l = Iarray.of_list l
