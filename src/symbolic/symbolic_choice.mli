150bf0b86fc1dec565f278bea0108e6e4aaa19c1
redianthus <leo@ocaml.pro>
2026-09-01 14:35:34 +0200

    refactor symbolic_choice.mli

--- src/symbolic/symbolic_choice.mli
+++ src/symbolic/symbolic_choice.mli
@@ -2,31 +2,16 @@
 (* Copyright © 2021-2026 OCamlPro *)
 (* Written by the Owi programmers *)
 
-type 'a t = ('a, Bug.t, Prio.metrics, Thread.t) Symex.Monad.t
-
-val return : 'a -> 'a t
+include
+  Choice_intf.S
+    with type 'a t = ('a, Bug.t, Prio.metrics, Thread.t) Symex.Monad.t
+     and type boolean := Symbolic_boolean.t
+     and type i32 := Symbolic_i32.t
+     and type value := Symbolic_value.t
 
 val prune : unit -> 'a t
 
-val ( let* ) : 'a t -> ('a -> 'b t) -> 'b t
-
-val ( let+ ) : 'a t -> ('a -> 'b) -> 'b t
-
-val trap : Result.err -> 'a t
-
-val select :
-     Symbolic_boolean.t
-  -> instr_counter_true:int option
-  -> instr_counter_false:int option
-  -> Bool.t t
-
-val select_i32 : Symbolic_i32.t -> Int32.t t
-
 val assertion : Symbolic_boolean.t -> unit t
-
-val assume : Symbolic_boolean.t -> unit t
-
-val assume_no_check : Symbolic_boolean.t -> unit t
 
 val map_state : (Thread.t -> Thread.t) -> unit t
 
@@ -36,16 +21,8 @@ val with_new_invisible_symbol : Smtml.Ty.t -> (Smtml.Symbol.t -> 'b) -> 'b t
 
 val with_new_symbol : Smtml.Ty.t -> (Smtml.Symbol.t -> 'b) -> 'b t
 
-val get_pc : unit -> Smtml.Expr.Set.t t
-
 val add_label : int * string -> unit t
 
 val open_scope : string -> unit t
 
 val close_scope : unit t
-
-val ite :
-     Symbolic_boolean.t
-  -> if_true:Symbolic_value.t
-  -> if_false:Symbolic_value.t
-  -> Symbolic_value.t t
