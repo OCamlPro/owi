32751d32ce80db6529ae4106eb4d49d6b01a3f9e
redianthus <leo@ocaml.pro>
2026-09-04 10:53:19 +0200

    add missing copyright headers

--- src/fuzz/fuzz_gen.ml
+++ src/fuzz/fuzz_gen.ml
@@ -1,3 +1,7 @@
+(* SPDX-License-Identifier: AGPL-3.0-or-later *)
+(* Copyright © 2021-2026 OCamlPro *)
+(* Written by the Owi programmers *)
+
 let i32 () =
   let use_dict = Random.bool () in
   let n =
