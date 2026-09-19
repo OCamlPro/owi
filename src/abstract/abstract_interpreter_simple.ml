395d7fb09d646677f36b9ffcfe4356ee1cc6e689
redianthus <leo@ocaml.pro>
2026-09-19 11:22:22 +0200

    fix compilation with bisect_ppx

--- src/abstract/abstract_interpreter_simple.ml
+++ src/abstract/abstract_interpreter_simple.ml
@@ -20,8 +20,8 @@ let add_divide_by_zero_invariant invariant ~uuid ~possible =
       ~message:"Possible division by zero"
 
 let eval_i32 env ({ stack; ctx; invariant; _ } as abs_state : Abstract_state.t)
-  uuid : Binary.i32_instr -> _ = function
-  | Const i ->
+  uuid = function
+  | (Const i : Binary.i32_instr) ->
     let stack = Stack.push_i32 stack (Abstract_i32.of_int32 ctx i) in
     let abs_state = { abs_state with stack } in
     State { abs_state; env }
